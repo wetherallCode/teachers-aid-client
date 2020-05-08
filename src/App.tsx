7ce060bb02edce44687f593cb2dffa260972d17b
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-05-08 16:42:13 -0400

    app now has react-router and a nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Modal } from './animations'
+import { useUserContextProvider } from './contexts/UserContext'
+import { Dashboard } from './dashboard/Dashboard'
+import { Home } from './home/Home'
+import { Login } from './home/Login'
+import { Nav } from './home/Nav'
+import { useToggle } from './hooks'
+// import { useLocation } from 'react-router'
+import {
+  Header,
+  // DashboardLink,
+  HomeLink,
+  UserNameHeader,
+} from './styled/headerStyles'
+import { LoginContainer, LoginToggle } from './styled/homeStyles'
+import { capitalizer } from './utils'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type LoginToggleProps = {
+  onClick: () => void
 }
 
-export default App;
+function App() {
+  const me = useUserContextProvider()
+  const [isLoginVisible, toggleLogin] = useToggle(false)
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  return (
+    <div>
+      <Header>
+        <HomeLink to='/'>MrWetherall.org</HomeLink>
+        <LoginContainer>
+          {!me ? (
+            <LoginToggle onClick={toggleLogin}>Login</LoginToggle>
+          ) : (
+            <UserNameHeader onClick={() => setIsNavOpen(true)}>
+              {me.__typename === 'Teacher'
+                ? `${capitalizer(me?.title) + '. ' + me?.lastName}`
+                : me.firstName}
+            </UserNameHeader>
+          )}
+
+          <Modal isToggled={isLoginVisible} setIsToggled={toggleLogin}>
+            <Login toggleLogin={toggleLogin} />
+          </Modal>
+        </LoginContainer>
+      </Header>
+      {me !== null && (
+        <Nav
+          isNavOpen={isNavOpen}
+          setIsNavOpen={setIsNavOpen}
+          toggleLogin={toggleLogin}
+          me={me}
+        />
+      )}
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='dashboard/*' element={<Dashboard />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
