5bb610c5ad9453860b88e2acfbcb7631233f8991
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-05-08 16:44:09 -0400

    added logout

---
+++ src/home/Logout.tsx
@@ -0,0 +1,35 @@
+import React, { FC, Dispatch, SetStateAction } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import { logout } from '../schemaTypes'
+import { LogoutLink } from '../styled/homeStyles'
+import { useNavigate } from 'react-router-dom'
+
+export const LOGOUT_MUTATION = gql`
+  mutation logout {
+    logout
+  }
+`
+export type LogoutProps = {
+  toggleLogin: () => void
+  setIsNavOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export const Logout: FC<LogoutProps> = ({ toggleLogin, setIsNavOpen }) => {
+  const navigate = useNavigate()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [logoutMutation, { data }] = useMutation<logout>(LOGOUT_MUTATION, {
+    onCompleted: () => toggleLogin,
+    refetchQueries: ['me'],
+  })
+  return (
+    <LogoutLink
+      onClick={() => {
+        logoutMutation()
+        navigate('/')
+        setIsNavOpen(false)
+      }}
+    >
+      Logout
+    </LogoutLink>
+  )
+}
