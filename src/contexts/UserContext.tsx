37aa787319f0b2827cf1cb19c795d616553ecabe
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-05-08 16:43:11 -0400

    user context tracks me query across entire app

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, FC, ReactNode, useContext } from 'react'
+import { me } from '../schemaTypes'
+import { useQuery, gql } from '@apollo/client'
+
+export const ME_QUERY = gql`
+  query me {
+    me {
+      userName
+      firstName
+      lastName
+      _id
+      ... on Teacher {
+        __typename
+        title
+      }
+      ... on Student {
+        __typename
+      }
+    }
+  }
+`
+
+const UserContext = createContext<any>(undefined)
+
+type UserContextProps = {
+  children: ReactNode
+}
+
+export const UserContextProvider: FC<UserContextProps> = ({ children }) => {
+  const { loading, error, data } = useQuery<me>(ME_QUERY)
+  if (loading) return <div>Loading </div>
+  if (error) console.error(error)
+
+  return (
+    <UserContext.Provider value={data?.me}>{children}</UserContext.Provider>
+  )
+}
+
+export function useUserContextProvider() {
+  const context = useContext(UserContext)
+  if (context === undefined) {
+    throw new Error(
+      'useCurrentPeriodState must be used within a UserContextProvider'
+    )
+  }
+  return context
+}
