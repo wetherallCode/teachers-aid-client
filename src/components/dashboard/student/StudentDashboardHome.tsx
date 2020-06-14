20b9edeef4281d0f37e4007c406fed92d22b2fa4
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-14 11:21:49 -0400

    added assignments route to StudentDashboard

--- src/components/dashboard/student/StudentDashboardHome.tsx
+++ src/components/dashboard/student/StudentDashboardHome.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { me_me_Student } from '../../../schemaTypes'
 import { useUserContextProvider } from '../../../contexts/UserContext'
+import { Routes, Route } from 'react-router'
+import { StudentAssignments } from './assignments/StudentAssignments'
+import { useParams } from 'react-router'
 
 export const StudentDashboardHome = () => {
-  const me = useUserContextProvider() as me_me_Student
-
-  return <div>{me.firstName}</div>
+  // const me: me_me_Student = useUserContextProvider()
+  const params = useParams()
+  console.log(params)
+  return (
+    <>
+      <Routes>
+        <Route path='assignments/*' element={<StudentAssignments />} />
+      </Routes>
+    </>
+  )
 }
