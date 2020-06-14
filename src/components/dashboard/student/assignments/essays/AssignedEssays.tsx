cdda6a0c6ef592e5190deca25b8f7beadfb9f132
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-14 11:21:07 -0400

    created AssignedEssays

---
+++ src/components/dashboard/student/assignments/essays/AssignedEssays.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import {
+  findEssaysToComplete,
+  findEssaysToCompleteVariables,
+  me_me_Student,
+} from '../../../../../schemaTypes'
+import { useUserContextProvider } from '../../../../../contexts/UserContext'
+
+export type AssignedEssaysProps = {}
+
+export const ESSAYS_TO_COMPLETE_QUERY = gql`
+  query findEssaysToComplete($input: FindEssaysToCompleteByStudentIdInput!) {
+    findEssaysToCompleteByStudentId(input: $input) {
+      essays {
+        _id
+        readings {
+          readingSections
+        }
+      }
+    }
+  }
+`
+
+export const AssignedEssays: FC<AssignedEssaysProps> = () => {
+  const me: me_me_Student = useUserContextProvider()
+  const { loading, error, data } = useQuery<
+    findEssaysToComplete,
+    findEssaysToCompleteVariables
+  >(ESSAYS_TO_COMPLETE_QUERY, {
+    variables: {
+      input: { studentId: me._id! },
+    },
+    onCompleted: (data) => console.log(data),
+    onError: (error) => console.error(error),
+  })
+
+  if (loading) return <div>Loading </div>
+  return (
+    <>
+      {data?.findEssaysToCompleteByStudentId.essays.map((essay) => (
+        <div key={essay._id!}>{essay.readings.readingSections}</div>
+      ))}
+    </>
+  )
+}
