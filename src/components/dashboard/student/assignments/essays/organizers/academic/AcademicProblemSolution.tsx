8bbd47cfe3bd8d70504d64c851747317aa1a264f
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-25 14:04:24 -0400

    added prelim Academic Organizer

---
+++ src/components/dashboard/student/assignments/essays/organizers/academic/AcademicProblemSolution.tsx
@@ -0,0 +1,129 @@
+import React, { FC, useEffect } from 'react'
+import { useStudentEssayContextProvider } from '../../StudentEssayContext'
+import { useMutation, gql } from '@apollo/client'
+import {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  updateProblemSolution,
+  updateProblemSolutionVariables,
+} from '../../../../../../../schemaTypes'
+
+export type AcademicProblemSolutionProps = {}
+
+export const UPDATE_PROBLEM_SOLUTION_MUTATION = gql`
+  mutation updateProblemSolution($input: UpdateProblemSolutionInput!) {
+    updateProblemSolution(input: $input) {
+      essay {
+        _id
+      }
+    }
+  }
+`
+
+export const AcademicProblemSolution: FC<AcademicProblemSolutionProps> = () => {
+  const [state, event] = useStudentEssayContextProvider()
+  const { subject } = state.context.academicOrganizer.academicSentenceStructure
+
+  const { problemSolution } = state.context.academicOrganizer.answer
+
+  const [updateProblemSolution] = useMutation<
+    updateProblemSolution,
+    updateProblemSolutionVariables
+  >(UPDATE_PROBLEM_SOLUTION_MUTATION, {
+    variables: {
+      input: {
+        essayId: state.context.essayId,
+        problem: state.context.academicOrganizer.answer.problemSolution.problem,
+        reasonForProblem:
+          state.context.academicOrganizer.answer.problemSolution
+            .reasonForProblem,
+        solvedBy:
+          state.context.academicOrganizer.answer.problemSolution.solvedBy,
+        whySolutionSolved:
+          state.context.academicOrganizer.answer.problemSolution
+            .whySolutionSolved,
+      },
+    },
+    onCompleted: (data) => console.log(data),
+    refetchQueries: ['findEssayById'],
+  })
+  useEffect(() => {
+    updateProblemSolution()
+  }, [
+    problemSolution,
+    updateProblemSolution,
+    // state.context.academicOrganizer.answer.problemSolution,
+  ])
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          event({ type: 'PREVIOUS' })
+          event({ type: 'SET_PRE_LOADED', payload: false })
+        }}
+      >
+        Change Question Type
+      </button>
+      <div>How: Problem and Solution</div>
+      <div>What is the problem for {subject}?</div>
+      <input
+        value={state.context.academicOrganizer.answer.problemSolution.problem}
+        onChange={(e: any) => {
+          event({
+            type: 'SET_PROBLEM_SOLUTION',
+            payload: { ...problemSolution, problem: e.target.value },
+          })
+        }}
+      />
+      <div>Why is this {subject}'s problem?</div>
+      <input
+        value={
+          state.context.academicOrganizer.answer.problemSolution
+            .reasonForProblem
+        }
+        onChange={(e: any) => {
+          event({
+            type: 'SET_PROBLEM_SOLUTION',
+            payload: {
+              ...problemSolution,
+              reasonForProblem: e.target.value,
+            },
+          })
+        }}
+      />
+      <div>How did {subject} solve the problem?</div>
+      <input
+        value={state.context.academicOrganizer.answer.problemSolution.solvedBy}
+        onChange={(e: any) => {
+          event({
+            type: 'SET_PROBLEM_SOLUTION',
+            payload: { ...problemSolution, solvedBy: e.target.value },
+          })
+        }}
+      />
+      <div>Why did the solution solve {subject}'s problem?</div>
+      <input
+        value={
+          state.context.academicOrganizer.answer.problemSolution
+            .whySolutionSolved
+        }
+        onChange={(e: any) => {
+          event({
+            type: 'SET_PROBLEM_SOLUTION',
+            payload: {
+              ...problemSolution,
+              whySolutionSolved: e.target.value,
+            },
+          })
+        }}
+      />
+      <button
+        onClick={() => {
+          event({ type: 'NEXT' })
+        }}
+      >
+        Next
+      </button>
+    </>
+  )
+}
---
+++ src/components/dashboard/student/assignments/essays/organizers/academic/AcademicWhyCauseEffect.tsx
@@ -0,0 +1,95 @@
+import React, { FC, useEffect } from 'react'
+import { useStudentEssayContextProvider } from '../../StudentEssayContext'
+import { useMutation, gql } from '@apollo/client'
+import {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  updateWhyCauseEffect,
+  updateWhyCauseEffectVariables,
+} from '../../../../../../../schemaTypes'
+
+export type AcademicWhyCauseEffectProps = {}
+
+export const UPDATE_WHY_CAUSE_EFFECT_MUTATION = gql`
+  mutation updateWhyCauseEffect($input: UpdateWhyCauseEffectInput!) {
+    updateWhyCauseEffect(input: $input) {
+      essay {
+        _id
+      }
+    }
+  }
+`
+
+export const AcademicWhyCauseEffect: FC<AcademicWhyCauseEffectProps> = () => {
+  const [state, event] = useStudentEssayContextProvider()
+
+  const { whyCauseEffect } = state.context.academicOrganizer.answer
+
+  const {
+    subject,
+    verb,
+  } = state.context.academicOrganizer.academicSentenceStructure
+
+  const [updateWhyCauseEffect] = useMutation<
+    updateWhyCauseEffect,
+    updateWhyCauseEffectVariables
+  >(UPDATE_WHY_CAUSE_EFFECT_MUTATION, {
+    variables: {
+      input: {
+        essayId: state.context.essayId,
+        ultimateCause:
+          state.context.academicOrganizer.answer.whyCauseEffect.ultimateCause,
+        proximateCause:
+          state.context.academicOrganizer.answer.whyCauseEffect.proximateCause,
+      },
+    },
+    onCompleted: () => console.log('update Complete'),
+    refetchQueries: ['findEssayById'],
+  })
+
+  useEffect(() => {
+    updateWhyCauseEffect()
+  }, [whyCauseEffect, updateWhyCauseEffect])
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          event({ type: 'PREVIOUS' })
+          event({ type: 'SET_PRE_LOADED', payload: false })
+        }}
+      >
+        Change Question Type
+      </button>
+      <div>Why: Cause and Effect</div>
+      <div>
+        Why did {subject} {verb}?
+      </div>
+      <input
+        type='text'
+        value={
+          state.context.academicOrganizer.answer.whyCauseEffect.proximateCause
+        }
+        onChange={(e: any) => {
+          event({
+            type: 'SET_WHY_CAUSE_EFFECT',
+            payload: { ...whyCauseEffect, proximateCause: e.target.value },
+          })
+        }}
+      />
+      <div>Why did that happen?</div>
+      <input
+        type='text'
+        value={
+          state.context.academicOrganizer.answer.whyCauseEffect.ultimateCause
+        }
+        onChange={(e: any) => {
+          event({
+            type: 'SET_WHY_CAUSE_EFFECT',
+            payload: { ...whyCauseEffect, ultimateCause: e.target.value },
+          })
+        }}
+      />
+      <button onClick={() => event({ type: 'NEXT' })}>Next</button>
+    </>
+  )
+}
