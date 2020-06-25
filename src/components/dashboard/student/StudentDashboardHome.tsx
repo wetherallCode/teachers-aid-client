d678982bf6609980020881d43ba2c3c66281c68f
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-25 14:06:31 -0400

    added essayToComplete page

--- src/components/dashboard/student/StudentDashboardHome.tsx
+++ src/components/dashboard/student/StudentDashboardHome.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { me_me_Student } from '../../../schemaTypes'
-import { useUserContextProvider } from '../../../contexts/UserContext'
 import { Routes, Route } from 'react-router'
 import { StudentAssignments } from './assignments/StudentAssignments'
-import { useParams } from 'react-router'
+import { EssayToComplete } from './assignments/essays/EssayToComplete'
+import { StudentEssayContextProvider } from './assignments/essays/StudentEssayContext'
 
 export const StudentDashboardHome = () => {
-  // const me: me_me_Student = useUserContextProvider()
-  const params = useParams()
-  console.log(params)
   return (
     <>
       <Routes>
         <Route path='assignments/*' element={<StudentAssignments />} />
+        <Route
+          path='assignments/:essayToComplete'
+          element={
+            <StudentEssayContextProvider>
+              <EssayToComplete />
+            </StudentEssayContextProvider>
+          }
+        />
       </Routes>
     </>
   )
