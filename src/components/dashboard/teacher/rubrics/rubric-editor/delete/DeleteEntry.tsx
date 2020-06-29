7acb856fe4b0b81c9b9be343256ae897cf70ef9a
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-06-29 14:30:01 -0400

    delete no longer in it's own state

---
+++ src/components/dashboard/teacher/rubrics/rubric-editor/delete/DeleteEntry.tsx
@@ -0,0 +1,46 @@
+import React, { FC, Dispatch, SetStateAction } from 'react'
+import { useRubricEditorContextProvider } from '../RubricEditorContext'
+import { gql, useMutation } from '@apollo/client'
+import {
+  removeRubricEntry,
+  removeRubricEntryVariables,
+} from '../../../../../../schemaTypes'
+
+export type DeleteEntryProps = {
+  setIsToggled: Dispatch<SetStateAction<boolean>>
+}
+
+export const REMOVE_RUBRIC_ENTRY_MUTATION = gql`
+  mutation removeRubricEntry($input: RemoveRubricEntryInput!) {
+    removeRubricEntry(input: $input) {
+      removed
+    }
+  }
+`
+
+export const DeleteEntry: FC<DeleteEntryProps> = ({ setIsToggled }) => {
+  const [state, event] = useRubricEditorContextProvider()
+  const [removeRubricEntry] = useMutation<
+    removeRubricEntry,
+    removeRubricEntryVariables
+  >(REMOVE_RUBRIC_ENTRY_MUTATION, {
+    variables: {
+      input: { rubricEntryId: state.context.selectedRubricEntry._id! },
+    },
+    onCompleted: (data) => console.log(data),
+    refetchQueries: ['findRubricEntries'],
+  })
+  return (
+    <>
+      <button onClick={() => setIsToggled(false)}>Don't Delete</button>
+      <button
+        onClick={() => {
+          removeRubricEntry()
+          event({ type: 'PREVIOUS' })
+        }}
+      >
+        Delete
+      </button>
+    </>
+  )
+}
