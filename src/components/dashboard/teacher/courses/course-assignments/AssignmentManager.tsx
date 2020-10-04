c690b8c3e02f7118bbc91829dd87ad5f3da96e95
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-10-04 17:02:05 -0400

    Added AssignmentManager Feature to look at and manipulate assignments

---
+++ src/components/dashboard/teacher/courses/course-assignments/AssignmentManager.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+export type AssignmentManagerProps = {}
+
+export const AssignmentManager: FC<AssignmentManagerProps> = () => {
+  return (
+    <>
+      <div>Assignment Manager</div>
+    </>
+  )
+}
