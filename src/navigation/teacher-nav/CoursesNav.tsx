7fdf54bed7c2eae711a0d2a42d4d0b7a43522846
Rob Wetherall <robertgwetherall1@gmail.com>
2021-02-15 19:17:41 -0500

    added tasks to courses nav

--- src/navigation/teacher-nav/CoursesNav.tsx
+++ src/navigation/teacher-nav/CoursesNav.tsx
@@ -9,20 +9,18 @@ import { me_me_Teacher } from '../../schemaTypes'
 import { useTeacherNavContextProvider } from './TeacherNavContext'
 import { sortByLetter } from '../../utils'
 
-export const CoursesNav: FC<TeacherNavProps> = ({ toggleLogin, setIsNavOpen }) => {
-	const me: me_me_Teacher = useUserContextProvider()
-	// console.log(me)
-	const [navState, event] = useTeacherNavContextProvider()
+export const CoursesNav: FC<TeacherNavProps> = ({
+  toggleLogin,
+  setIsNavOpen,
+}) => {
+  const me: me_me_Teacher = useUserContextProvider()
+  const [navState, event] = useTeacherNavContextProvider()
 
-	const coursesToNavigate = me.teachesCourses.slice(1).sort(sortByLetter)
+  const coursesToNavigate = me.teachesCourses.slice(1).sort(sortByLetter)
 
-	return (
-		<>
-			<motion.ul variants={ulVariants}>
-				{/* <motion.li variants={liVariants}>
-          <Link to='dashboard/courses/lesson-editor'>Lesson Editor</Link>
-        </motion.li> */}
-<<<<<<< HEAD
+  return (
+    <>
+      <motion.ul variants={ulVariants}>
         {navState.matches('courses.home.idle') && (
           <>
             {me.teachesCourses.map((course) => (
@@ -106,68 +104,4 @@ export const CoursesNav: FC<TeacherNavProps> = ({ toggleLogin, setIsNavOpen }) =
       </motion.ul>
     </>
   )
-=======
-				{navState.matches('courses.home.idle') && (
-					<>
-						{coursesToNavigate.map((course) => (
-							<motion.li key={course._id!} variants={liVariants}>
-								<Link
-									to={`dashboard/courses/${course._id}`}
-									onClick={() => {
-										event({ type: 'SET_COURSE', payload: course._id! })
-										event({ type: 'COURSE_SELECT' })
-									}}>
-									{course.name}
-								</Link>
-							</motion.li>
-						))}
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/create-course`}>New Course</Link>
-						</motion.li>
-					</>
-				)}
-				{navState.matches('courses.home.courseSelect') && (
-					<>
-						<motion.li variants={liVariants}>
-							<Link to={'dashboard/courses'} onClick={() => event({ type: 'COURSES' })}>
-								Courses
-							</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/lesson-editor`}>
-								Lesson Editor
-							</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/assign-assignments`}>
-								Assign Work
-							</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/assignment-manager`}>
-								Assignments
-							</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/roster`}>Roster</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/view-reading-guide-data`}>
-								ReadingGuide Data
-							</Link>
-						</motion.li>
-						<motion.li variants={liVariants}>
-							<Link to={`dashboard/courses/${navState.context.course}/edit-course`}>
-								Edit Course
-							</Link>
-						</motion.li>
-					</>
-				)}
-				<motion.li variants={liVariants}>
-					<Logout toggleLogin={toggleLogin} setIsNavOpen={setIsNavOpen} />
-				</motion.li>
-			</motion.ul>
-		</>
-	)
->>>>>>> 7dac1573e5f6b2f259cbb701e5f43f494f34f25c
 }
