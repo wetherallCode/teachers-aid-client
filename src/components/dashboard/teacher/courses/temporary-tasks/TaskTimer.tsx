5e862cd8e1e9cb8d8e39a71e4ca6fec6b67ace60
Rob Wetherall <robertgwetherall1@gmail.com>
2021-02-25 10:20:54 -0500

    Adding new task resets timer

--- src/components/dashboard/teacher/courses/temporary-tasks/TaskTimer.tsx
+++ src/components/dashboard/teacher/courses/temporary-tasks/TaskTimer.tsx
@@ -1,47 +1,57 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import { useTemporaryTasksContextProvider } from './state-n-styles/TemporaryTasksContext'
 import {
-  IncreaseTaskTimer,
-  ReduceTaskTimer,
-  StartTaskTimer,
-  TaskTimerContainer,
-  TaskTimerPresetContainer,
-  TaskTimerPresetDisplay,
+	IncreaseTaskTimer,
+	ReduceTaskTimer,
+	StartTaskTimer,
+	TaskTimerContainer,
+	TaskTimerPresetContainer,
+	TaskTimerPresetDisplay,
 } from './state-n-styles/temporaryTaskStyles'
 import { TaskTimerDisplay } from './TaskTimerDisplay'
 
 export type TaskTimerProps = {}
 
 export const TaskTimer: FC<TaskTimerProps> = () => {
-  const [presetMinuteValue, setPresetMinuteValue] = useState(5)
-  const [startTimer, setStartTimer] = useState(false)
-  return (
-    <TaskTimerContainer>
-      {!startTimer ? (
-        <TaskTimerPresetContainer>
-          <ReduceTaskTimer
-            onClick={() => {
-              if (presetMinuteValue > 0) setPresetMinuteValue((c) => c - 1)
-            }}
-          >
-            <div>-</div>
-          </ReduceTaskTimer>
-          <TaskTimerPresetDisplay>
-            <div>{presetMinuteValue}</div>
-          </TaskTimerPresetDisplay>
-          <IncreaseTaskTimer onClick={() => setPresetMinuteValue((c) => c + 1)}>
-            <div>+</div>
-          </IncreaseTaskTimer>
+	const [state, event] = useTemporaryTasksContextProvider()
+	const [presetMinuteValue, setPresetMinuteValue] = useState(
+		state.context.taskNumber === 0 ? 10 : 5
+	)
+	const [startTimer, setStartTimer] = useState(false)
 
-          <StartTaskTimer onClick={() => setStartTimer(true)}>
-            <div>Start </div>
-          </StartTaskTimer>
-        </TaskTimerPresetContainer>
-      ) : (
-        <TaskTimerDisplay
-          presetTime={presetMinuteValue * 1000 * 60}
-          setStartTimer={setStartTimer}
-        />
-      )}
-    </TaskTimerContainer>
-  )
+	useEffect(() => {
+		if (startTimer) {
+			setStartTimer(false)
+		}
+	}, [state.context.taskNumber])
+
+	return (
+		<TaskTimerContainer>
+			{!startTimer ? (
+				<TaskTimerPresetContainer>
+					<ReduceTaskTimer
+						onClick={() => {
+							if (presetMinuteValue > 0) setPresetMinuteValue((c) => c - 1)
+						}}>
+						<div>-</div>
+					</ReduceTaskTimer>
+					<TaskTimerPresetDisplay>
+						<div>{presetMinuteValue}</div>
+					</TaskTimerPresetDisplay>
+					<IncreaseTaskTimer onClick={() => setPresetMinuteValue((c) => c + 1)}>
+						<div>+</div>
+					</IncreaseTaskTimer>
+
+					<StartTaskTimer onClick={() => setStartTimer(true)}>
+						<div>Start </div>
+					</StartTaskTimer>
+				</TaskTimerPresetContainer>
+			) : (
+				<TaskTimerDisplay
+					presetTime={presetMinuteValue * 1000 * 60}
+					setStartTimer={setStartTimer}
+				/>
+			)}
+		</TaskTimerContainer>
+	)
 }
