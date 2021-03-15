e73cd1bcf938d9ace89d19c2e646dea1a7e51c43
Rob Wetherall <robertgwetherall1@gmail.com>
2021-03-15 06:28:23 -0400

    modified

--- src/components/dashboard/teacher/assignments/create-assignments/CreateAssignment.tsx
+++ src/components/dashboard/teacher/assignments/create-assignments/CreateAssignment.tsx
@@ -32,78 +32,89 @@ export const CreateAssignment: FC<CreateAssignmentProps> = () => {
   const [state, event] = useCreateAssignmentContextPovider()
   const me: me_me_Teacher = useUserContextProvider()
   const { teachesCourses } = me
-
+  console.log(state.value)
   return (
     <CreateAssignmentsContainer>
       <CourseSelectContainer>
-        <div>Select Course</div>
-        {teachesCourses
-          .slice(1)
-          .sort(sortByLetter)
-          .map((course) => (
-            <CourseToSelect
-              key={course._id!}
-              onClick={() => {
-                event({ type: 'SET_COURSE_ID', payload: course._id! })
-                setCourseId(course._id!)
-                event({ type: 'IDLE' })
-              }}
-              style={
-                course._id === courseId
-                  ? { textDecoration: 'underline' }
-                  : { textDecoration: 'none' }
-              }
-            >
-              {course.name}
-            </CourseToSelect>
-          ))}
+        <CourseSelect>
+          <div>Select Course</div>
+          {teachesCourses
+            .slice(1)
+            .sort(sortByLetter)
+            .map((course) => (
+              <CourseToSelect
+                key={course._id!}
+                onClick={() => {
+                  event({ type: 'SET_COURSE_ID', payload: course._id! })
+                  setCourseId(course._id!)
+                  event({ type: 'ESSAY' })
+                }}
+                style={
+                  course._id === courseId
+                    ? { textDecoration: 'underline' }
+                    : { textDecoration: 'none' }
+                }
+              >
+                {course.name}
+              </CourseToSelect>
+            ))}
+        </CourseSelect>
       </CourseSelectContainer>
-      <CreateAssignmentTitle>
-        <div>Create Assignments</div>
-      </CreateAssignmentTitle>
-
-      {courseId && (
-        <AssignmentTypeSelectorContainer>
-          <AssignmentType selected={state.matches('essay')}>
-            <div
-              onClick={() => {
-                event({ type: 'ESSAY' })
-              }}
-            >
-              Essay
+      <AssignmentCreatorContainer>
+        <CreateAssignmentTitle>
+          <div>Create Assignments</div>
+        </CreateAssignmentTitle>
+        <div>
+          {courseId && (
+            <div>
+              <AssignmentTypeSelectorContainer>
+                <AssignmentType selected={state.matches('essay')}>
+                  <div
+                    onClick={() => {
+                      event({ type: 'ESSAY' })
+                    }}
+                  >
+                    Essay
+                  </div>
+                </AssignmentType>
+                <AssignmentType selected={state.matches('readingGuide')}>
+                  <div onClick={() => event({ type: 'READING_GUIDE' })}>
+                    Reading Guide
+                  </div>
+                </AssignmentType>
+              </AssignmentTypeSelectorContainer>
             </div>
-          </AssignmentType>
-          <AssignmentType selected={state.matches('readingGuide')}>
-            <div onClick={() => event({ type: 'READING_GUIDE' })}>
-              Reading Guide
-            </div>
-          </AssignmentType>
-        </AssignmentTypeSelectorContainer>
-      )}
-      {courseId && (
-        <>
-          {state.matches('essay') && (
-            <AssignmentCreatorContainer>
-              {state.matches('essay.unit') && <EssayUnitSelect />}
-              {state.matches('essay.lesson') && (
-                <EssayLessonSelect courseId={courseId} />
-              )}
-              {state.matches('essay.essayInfo') && <EssayLessonInfo me={me} />}
-            </AssignmentCreatorContainer>
           )}
-          {state.matches('readingGuide') && (
-            <AssignmentCreatorContainer>
-              {state.matches('readingGuide.unit') && <ReadingGuideUnitSelect />}
-              {state.matches('readingGuide.lesson') && (
-                <ReadingGuideLessonSelect />
+          {courseId && (
+            <>
+              {state.matches('essay') && (
+                <>
+                  {state.matches('essay.unit') && <EssayUnitSelect />}
+                  {state.matches('essay.lesson') && (
+                    <EssayLessonSelect courseId={courseId} />
+                  )}
+                  {state.matches('essay.essayInfo') && (
+                    <EssayLessonInfo me={me} />
+                  )}
+                </>
               )}
-              {state.matches('readingGuide.readingGuideInfo') && (
-                <ReadingGuideLessonInfo me={me} />
+              {state.matches('readingGuide') && (
+                <div>
+                  {state.matches('readingGuide.unit') && (
+                    <ReadingGuideUnitSelect />
+                  )}
+                  {state.matches('readingGuide.lesson') && (
+                    <ReadingGuideLessonSelect />
+                  )}
+                  {state.matches('readingGuide.readingGuideInfo') && (
+                    <ReadingGuideLessonInfo me={me} />
+                  )}
+                </div>
               )}
-            </AssignmentCreatorContainer>
+            </>
           )}
-        </>
-      )}
+        </div>
+      </AssignmentCreatorContainer>
     </CreateAssignmentsContainer>
   )
 }
