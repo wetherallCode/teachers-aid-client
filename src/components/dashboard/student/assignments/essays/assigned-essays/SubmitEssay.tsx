d7b26b372f51e8676ec4ab9f789a84a9c4e2b566
Rob Wetherall <robertgwetherall1@gmail.com>
2021-03-31 12:22:37 -0400

    submittal times are on the onClick handler

--- src/components/dashboard/student/assignments/essays/assigned-essays/SubmitEssay.tsx
+++ src/components/dashboard/student/assignments/essays/assigned-essays/SubmitEssay.tsx
@@ -2,130 +2,128 @@ import React, { FC, useState } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import { useNavigate } from 'react-router-dom'
 import {
-  submitEssayFinalDraft,
-  submitEssayFinalDraftVariables,
-  SubmittedFinalDraftsInput,
-  findEssayById_findEssayById_essay,
+	submitEssayFinalDraft,
+	submitEssayFinalDraftVariables,
+	SubmittedFinalDraftsInput,
+	findEssayById_findEssayById_essay,
 } from '../../../../../../schemaTypes'
 import {
-  OrganizerControlButton,
-  EssaySubmitButton,
-  SubmitEssayModalContainer,
-  SubmitEssayModalSubmitButton,
-  SubmitEssayModal,
-  SubmitEssayModalMessage,
-  EssaySubmitCheck,
+	OrganizerControlButton,
+	EssaySubmitButton,
+	SubmitEssayModalContainer,
+	SubmitEssayModalSubmitButton,
+	SubmitEssayModal,
+	SubmitEssayModalMessage,
+	EssaySubmitCheck,
 } from './state-and-styles/assignedEssayStyles'
 import { Modal } from '../../../../../../animations'
 import { useStudentEssayContextProvider } from './state-and-styles/StudentEssayContext'
 
 export const SUBMIT_FINAL_DRAFT_MUTATION = gql`
-  mutation submitEssayFinalDraft($input: SubmitEssayFinalDraftInput!) {
-    submitEssayFinalDraft(input: $input) {
-      essay {
-        _id
-        assigned
-        finalDraft {
-          submittedFinalDraft {
-            gradingDraft
-            draft
-          }
-        }
-      }
-    }
-  }
+	mutation submitEssayFinalDraft($input: SubmitEssayFinalDraftInput!) {
+		submitEssayFinalDraft(input: $input) {
+			essay {
+				_id
+				assigned
+				finalDraft {
+					submittedFinalDraft {
+						gradingDraft
+						draft
+					}
+				}
+			}
+		}
+	}
 `
 
 export type SubmitEssayFinalDraftInput = {
-  _id: string
-  submittedFinalDraft: SubmittedFinalDraftsInput
-  // essay: findEssayById_findEssayById_essay
-  response: boolean
+	_id: string
+	submittedFinalDraft: SubmittedFinalDraftsInput
+	// essay: findEssayById_findEssayById_essay
+	response: boolean
 }
 
 export const SubmitEssay: FC<SubmitEssayFinalDraftInput> = ({
-  _id,
-  submittedFinalDraft,
-  response,
+	_id,
+	submittedFinalDraft,
+	response,
 }) => {
-  const navigate = useNavigate()
-  const [, event] = useStudentEssayContextProvider()
-  const [submitToggle, setSubmitToggle] = useState(false)
-  const [submitFinalDraft, { called }] = useMutation<
-    submitEssayFinalDraft,
-    submitEssayFinalDraftVariables
-  >(SUBMIT_FINAL_DRAFT_MUTATION, {
-    variables: {
-      input: {
-        _id,
-        submittedFinalDraft,
-        submitTime: new Date().toLocaleString(),
-        late: true, //server will change based on time submitted
-        paperBased: false,
-      },
-    },
-    onCompleted: () => navigate('/dashboard/assignments'),
-    refetchQueries: ['findEssaysToComplete', 'findEssayById'],
-  })
+	const navigate = useNavigate()
+	const [, event] = useStudentEssayContextProvider()
+	const [submitToggle, setSubmitToggle] = useState(false)
+	const [submitFinalDraft, { called }] = useMutation<
+		submitEssayFinalDraft,
+		submitEssayFinalDraftVariables
+	>(SUBMIT_FINAL_DRAFT_MUTATION, {
+		// variables: {
+		// 	input: {
+		// 		_id,
+		// 		submittedFinalDraft,
+		// 		submitTime: new Date().toLocaleString(),
+		// 		late: true, //server will change based on time submitted
+		// 		paperBased: false,
+		// 	},
+		// },
+		onCompleted: () => {
+			console.log(new Date().toLocaleString())
+			navigate('/dashboard/assignments')
+		},
+		refetchQueries: ['findEssaysToComplete', 'findEssayById'],
+	})
 
-  return (
-    <>
-      {!submitToggle ? (
-        <EssaySubmitButton
-          color={'var(--blue)'}
-          submitFinal={submitToggle}
-          onClick={() => {
-            event({ type: 'PREVIOUS' })
-            event({ type: 'NEXT' })
-          }}
-        >
-          Go Back
-        </EssaySubmitButton>
-      ) : (
-        <EssaySubmitButton
-          color={'var(--red)'}
-          onClick={() => setSubmitToggle(false)}
-        >
-          No
-        </EssaySubmitButton>
-      )}
-      {submitToggle && <EssaySubmitCheck>Are you Sure?</EssaySubmitCheck>}
-      {response && (
-        <>
-          {!submitToggle ? (
-            <EssaySubmitButton
-              color={'var(--blue)'}
-              submitFinal={submitToggle}
-              onClick={() => setSubmitToggle(true)}
-            >
-              Submit
-            </EssaySubmitButton>
-          ) : (
-            <EssaySubmitButton
-              color={called ? 'var(--grey)' : 'var(--blue)'}
-              submitFinal={submitToggle}
-              onClick={() => {
-                if (!called) {
-                  submitFinalDraft()
-                }
-              }}
-            >
-              {called ? 'Submiting' : 'Yes'}
-            </EssaySubmitButton>
-          )}
-        </>
-      )}
-      {/* <SubmitEssayModal isToggled={isToggled} setIsToggled={setIsToggled}>
-        <SubmitEssayModalContainer>
-          <SubmitEssayModalMessage>
-            Are you sure? once it's been submitted it's final.
-          </SubmitEssayModalMessage>
-          <SubmitEssayModalSubmitButton>Not Yet!</SubmitEssayModalSubmitButton>
-          <SubmitEssayModalSubmitButton onClick={() => submitFinalDraft()}>
-            Submit
-          </SubmitEssayModalSubmitButton>
-        </SubmitEssayModalContainer>
-      </SubmitEssayModal> */}
-    </>
-  )
+	const handleSubmit = () => {
+		if (!called) {
+			submitFinalDraft({
+				variables: {
+					input: {
+						_id,
+						submittedFinalDraft,
+						submitTime: new Date().toLocaleString(),
+						late: true, //server will change based on time submitted
+						paperBased: false,
+					},
+				},
+			})
+		}
+	}
+
+	return (
+		<>
+			{!submitToggle ? (
+				<EssaySubmitButton
+					color={'var(--blue)'}
+					submitFinal={submitToggle}
+					onClick={() => {
+						event({ type: 'PREVIOUS' })
+						event({ type: 'NEXT' })
+					}}>
+					Go Back
+				</EssaySubmitButton>
+			) : (
+				<EssaySubmitButton color={'var(--red)'} onClick={() => setSubmitToggle(false)}>
+					No
+				</EssaySubmitButton>
+			)}
+			{submitToggle && <EssaySubmitCheck>Are you Sure?</EssaySubmitCheck>}
+			{response && (
+				<>
+					{!submitToggle ? (
+						<EssaySubmitButton
+							color={'var(--blue)'}
+							submitFinal={submitToggle}
+							onClick={() => setSubmitToggle(true)}>
+							Submit
+						</EssaySubmitButton>
+					) : (
+						<EssaySubmitButton
+							color={called ? 'var(--grey)' : 'var(--blue)'}
+							submitFinal={submitToggle}
+							onClick={handleSubmit}>
+							{called ? 'Submiting' : 'Yes'}
+						</EssaySubmitButton>
+					)}
+				</>
+			)}
+		</>
+	)
 }
--- src/components/dashboard/student/assignments/readingGuides/SubmitReadingGuide.tsx
+++ src/components/dashboard/student/assignments/readingGuides/SubmitReadingGuide.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  submitReadingGuide,
-  submitReadingGuideVariables,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	submitReadingGuide,
+	submitReadingGuideVariables,
 } from '../../../../../schemaTypes'
 import { useNavigate } from 'react-router'
 import { useReadingGuideToCompleteContextProvider } from './state-and-styles/ReadingGuideToCompleteContext'
@@ -12,38 +12,45 @@ import { SubmitReadingGuideButton } from './state-and-styles/readingGuideStyles'
 export type SubmitReadingGuideProps = {}
 
 export const SUBMIT_READING_GUIDE_MUTATION = gql`
-  mutation submitReadingGuide($input: SubmitReadingGuideInput!) {
-    submitReadingGuide(input: $input) {
-      readingGuide {
-        _id
-      }
-    }
-  }
+	mutation submitReadingGuide($input: SubmitReadingGuideInput!) {
+		submitReadingGuide(input: $input) {
+			readingGuide {
+				_id
+			}
+		}
+	}
 `
 
 export const SubmitReadingGuide: FC<SubmitReadingGuideProps> = () => {
-  const navigate = useNavigate()
-  const [state] = useReadingGuideToCompleteContextProvider()
+	const navigate = useNavigate()
+	const [state] = useReadingGuideToCompleteContextProvider()
 
-  const [submitReadingGuide] = useMutation<
-    submitReadingGuide,
-    submitReadingGuideVariables
-  >(SUBMIT_READING_GUIDE_MUTATION, {
-    variables: {
-      input: {
-        ...state.context.submitReadingGuideInputs,
-        submitTime: new Date().toLocaleString(),
-      },
-    },
-    onCompleted: () => navigate('/dashboard/assignments'),
-    refetchQueries: ['findReadingGuidesToComplete', 'findReadingGuideById'],
-  })
+	const [submitReadingGuide] = useMutation<submitReadingGuide, submitReadingGuideVariables>(
+		SUBMIT_READING_GUIDE_MUTATION,
+		{
+			onCompleted: () => {
+				console.log(new Date().toLocaleString())
+				navigate('/dashboard/assignments')
+			},
+			refetchQueries: ['findReadingGuidesToComplete', 'findReadingGuideById'],
+		}
+	)
 
-  return (
-    <>
-      <SubmitReadingGuideButton onClick={() => submitReadingGuide()}>
-        Submit
-      </SubmitReadingGuideButton>
-    </>
-  )
+	return (
+		<>
+			<SubmitReadingGuideButton
+				onClick={() =>
+					submitReadingGuide({
+						variables: {
+							input: {
+								...state.context.submitReadingGuideInputs,
+								submitTime: new Date().toLocaleString(),
+							},
+						},
+					})
+				}>
+				Submit
+			</SubmitReadingGuideButton>
+		</>
+	)
 }
