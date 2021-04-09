95e66458f6bcf9ae68facca7aed70b20123dd838
Rob Wetherall <robertgwetherall1@gmail.com>
2021-04-09 15:18:54 -0400

    underlined course name is styled components

--- src/components/dashboard/teacher/assignments/article-reviews/review-articleReviews/ReviewArticleReviews.tsx
+++ src/components/dashboard/teacher/assignments/article-reviews/review-articleReviews/ReviewArticleReviews.tsx
@@ -6,64 +6,62 @@ import { useToggle } from '../../../../../../hooks'
 import { MarkingPeriodEnum, me_me_Teacher } from '../../../../../../schemaTypes'
 import { sortByLetter } from '../../../../../../utils'
 import {
-	CourseSelect,
-	CourseToSelect,
+  CourseSelect,
+  CourseToSelect,
 } from '../../grade-assignments/state-n-styles/GradeEssayContainerStyles'
 import { useArticleReviewContextProvider } from '../state-styles/ArticleReviewContext'
 import {
-	NoCourseDisplay,
-	ReviewerContainer,
-	ReviewerCourseSelect,
-	ReviewerCourseSelectBack,
-	ReviewerCourseSelectContainer,
+  NoCourseDisplay,
+  ReviewerContainer,
+  ReviewerCourseSelect,
+  ReviewerCourseSelectBack,
+  ReviewerCourseSelectContainer,
 } from '../state-styles/articleReviewStyles'
 import { ReviewDisplay } from './ReviewDisplay'
 
 export type ReviewArticleReviewsProps = {}
 
 export const ReviewArticleReviews: FC<ReviewArticleReviewsProps> = () => {
-	const me: me_me_Teacher = useUserContextProvider()
-	const [state, event] = useArticleReviewContextProvider()
-	// const { markingPeriodEnum } = useEnumContextProvider()
-	// const [markingPeriodState] = useMarkingPeriodContextProvider()
-	// const [mp, setMp] = useState(markingPeriodState.context.currentMarkingPeriod)
+  const me: me_me_Teacher = useUserContextProvider()
+  const [state, event] = useArticleReviewContextProvider()
 
-	const courses = me.teachesCourses.filter((course) => course.name !== 'Cohort Class')
-	const fakeCourse = me.teachesCourses.filter((course) => course.name === 'Cohort Class')
+  const courses = me.teachesCourses.filter(
+    (course) => course.name !== 'Cohort Class'
+  )
+  const fakeCourse = me.teachesCourses.filter(
+    (course) => course.name === 'Cohort Class'
+  )
 
-	return (
-		<ReviewerContainer>
-			<ReviewerCourseSelectContainer>
-				<CourseSelect>
-					<div>Select Course</div>
-					{courses.sort(sortByLetter).map((course) => {
-						return (
-							<CourseToSelect
-								key={course._id!}
-								style={
-									course._id === state.context.courseToReview
-										? { textDecoration: 'underline' }
-										: { textDecoration: 'none' }
-								}
-								onClick={() => {
-									event({ type: 'SET_COURSE_TO_REVIEW', payload: course._id! })
-								}}>
-								{course.name}
-							</CourseToSelect>
-						)
-					})}
-				</CourseSelect>
-				<ReviewerCourseSelectBack>
-					<div onClick={() => event({ type: 'IDLE' })}>Back</div>
-				</ReviewerCourseSelectBack>
-			</ReviewerCourseSelectContainer>
-			{state.context.courseToReview ? (
-				<ReviewDisplay />
-			) : (
-				<NoCourseDisplay>
-					<div>Select a Course to Review</div>
-				</NoCourseDisplay>
-			)}
-		</ReviewerContainer>
-	)
+  return (
+    <ReviewerContainer>
+      <ReviewerCourseSelectContainer>
+        <CourseSelect>
+          <div>Select Course</div>
+          {courses.sort(sortByLetter).map((course) => {
+            return (
+              <CourseToSelect
+                key={course._id!}
+                selected={course._id === state.context.courseToReview}
+                onClick={() => {
+                  event({ type: 'SET_COURSE_TO_REVIEW', payload: course._id! })
+                }}
+              >
+                {course.name}
+              </CourseToSelect>
+            )
+          })}
+        </CourseSelect>
+        <ReviewerCourseSelectBack>
+          <div onClick={() => event({ type: 'IDLE' })}>Back</div>
+        </ReviewerCourseSelectBack>
+      </ReviewerCourseSelectContainer>
+      {state.context.courseToReview ? (
+        <ReviewDisplay />
+      ) : (
+        <NoCourseDisplay>
+          <div>Select a Course to Review</div>
+        </NoCourseDisplay>
+      )}
+    </ReviewerContainer>
+  )
 }
