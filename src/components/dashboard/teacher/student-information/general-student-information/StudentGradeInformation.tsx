bd660b7f5460184f8eede04d5de496c83f5a5822
Rob Wetherall <robertgwetherall1@gmail.com>
2021-04-30 19:14:16 -0400

    deleted unused code

--- src/components/dashboard/teacher/student-information/general-student-information/StudentGradeInformation.tsx
+++ src/components/dashboard/teacher/student-information/general-student-information/StudentGradeInformation.tsx
@@ -1,22 +1,10 @@
-import { useQuery } from '@apollo/client'
 import React from 'react'
 import { useGradeCalculator } from '../../../../../hooks/useGradeCalculator'
 import {
 	findAllStudentsForStudentInformation_findAllStudents_students,
-	findAssignmentByStudentId,
-	findAssignmentByStudentIdVariables,
-	findResponsibilityPointsByStudentId_findResponsibilityPointsByStudentId_responsibilityPoints,
 	MarkingPeriodEnum,
 } from '../../../../../schemaTypes'
-import {
-	letterGrade,
-	primaryGradeCalculator,
-	responsibilityPointConverter,
-	secondaryGradeCalculator,
-	supportiveGradeCalculator,
-	totalGrade,
-} from '../../../../../utils'
-import { FIND_ASSINGMENT_INFORMATION_QUERY } from '../AssignmentInformation'
+import { letterGrade } from '../../../../../utils'
 
 export type StudentGradeInformationProps = {
 	student: findAllStudentsForStudentInformation_findAllStudents_students
@@ -27,17 +15,21 @@ export const StudentGradeInformation = ({
 	student,
 	selectedMarkingPeriod,
 }: StudentGradeInformationProps) => {
-	const { grade: calculatedGrade, loading } = useGradeCalculator(
+	const { grade: calculatedGrade, loading, noGrade } = useGradeCalculator(
 		student._id!,
 		selectedMarkingPeriod
 	)
 
 	return (
 		<>
-			<div>
-				Grade: {loading ? 'loading' : calculatedGrade + '%'}{' '}
-				{!loading && letterGrade(calculatedGrade)}
-			</div>
+			{!noGrade ? (
+				<div>
+					Grade: {loading ? 'loading' : calculatedGrade + '%'}{' '}
+					{!loading && letterGrade(calculatedGrade)}
+				</div>
+			) : (
+				<div>Grade: No Grades Yet</div>
+			)}
 		</>
 	)
 }
