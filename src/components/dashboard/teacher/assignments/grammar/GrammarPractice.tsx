fa96cc0b7d41222c54a2e9e826037fed2f7fe39f
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-07 06:44:05 -0400

    wip

--- src/components/dashboard/teacher/assignments/grammar/GrammarPractice.tsx
+++ src/components/dashboard/teacher/assignments/grammar/GrammarPractice.tsx
@@ -1,51 +1,81 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useSelectedText } from '../../../../../hooks/useSelectedText'
 
 export type GrammarPracticeProps = {}
 
 export const GrammarPractice = ({}: GrammarPracticeProps) => {
-	const [select, text, reset] = useSelectedText()
-	const [state, setState] = useState<'subject' | 'predicate' | 'final'>('subject')
-	const [subject, setSubject] = useState('')
-	const [predicate, setPredicate] = useState('')
+  const [select, text, reset] = useSelectedText()
+  const [state, setState] = useState<'subject' | 'predicate' | 'final'>(
+    'subject'
+  )
+  const [subject, setSubject] = useState('')
+  const [predicate, setPredicate] = useState('')
+  let sentence = 'A good player respects their team.'
+  let newSentence = [sentence]
+  let partialSentence =
+    subject && sentence.indexOf(subject) + ', ' + subject.length
 
-	return (
-		<>
-			<div onMouseUp={select}>1. A good player respects their team.</div>
-			{state === 'subject' && (
-				<div>
-					<div>Subject: {subject ? subject : text}</div>
-					<button
-						onClick={() => {
-							setSubject(text)
-							reset()
-							setState('predicate')
-						}}>
-						Set
-					</button>
-				</div>
-			)}
-			{state === 'predicate' && (
-				<div>
-					<div>Predicate: {predicate ? predicate : text}</div>
-					<button
-						onClick={() => {
-							setPredicate(text)
-							reset()
-							setState('final')
-						}}>
-						Set
-					</button>
-				</div>
-			)}
-			{state === 'final' && (
-				<div>
-					<span style={{ textDecoration: 'underline' }}>{subject}</span> |{' '}
-					<span style={{ textDecoration: 'underline', textDecorationStyle: 'double' }}>
-						{predicate}
-					</span>
-				</div>
-			)}
-		</>
-	)
+  return (
+    <>
+      <div onMouseUp={select}>
+        {newSentence.map((part) => (
+          <span
+            style={
+              subject
+                ? { textDecoration: 'underline' }
+                : predicate
+                ? { textDecoration: 'underline', textDecorationStyle: 'double' }
+                : {}
+            }
+            key={part}
+          >
+            {part}
+          </span>
+        ))}
+      </div>
+      {state === 'subject' && (
+        <div>
+          <div>Subject: {subject ? subject : text}</div>
+          <button
+            onClick={() => {
+              setSubject(text)
+
+              //   newSentence(subject)
+              reset()
+              setState('predicate')
+            }}
+          >
+            Set
+          </button>
+        </div>
+      )}
+      {state === 'predicate' && (
+        <div>
+          <div>Predicate: {predicate ? predicate : text}</div>
+          <button
+            onClick={() => {
+              setPredicate(text)
+              reset()
+              setState('final')
+            }}
+          >
+            Set
+          </button>
+        </div>
+      )}
+      {state === 'final' && (
+        <div>
+          <span style={{ textDecoration: 'underline' }}>{subject}</span> |{' '}
+          <span
+            style={{
+              textDecoration: 'underline',
+              textDecorationStyle: 'double',
+            }}
+          >
+            {predicate}
+          </span>
+        </div>
+      )}
+    </>
+  )
 }
