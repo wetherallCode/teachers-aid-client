706cb2f1d18d3710f5c1f1b51bfd5ebd76e669a3
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-12 06:47:53 -0400

    trying to split the period off from the last word

--- src/components/dashboard/teacher/development/grammar/SimpleSubjectPredicate.tsx
+++ src/components/dashboard/teacher/development/grammar/SimpleSubjectPredicate.tsx
@@ -4,98 +4,144 @@ import { simplePredicateGrader } from './simplePredicateGrader'
 import { simpleSubjectGrader } from './simpleSubGrader'
 
 export type SimpleSubjectPredicateProps = {
-	sentence: string
-	setSentence: Dispatch<SetStateAction<string>>
+  sentence: string
+  setSentence: Dispatch<SetStateAction<string>>
 }
 
-export const SimpleSubjectPredicate = ({ sentence, setSentence }: SimpleSubjectPredicateProps) => {
-	const [select, text, reset] = useSelectedText()
-	const [state, setState] = useState<'subject' | 'predicate' | 'final'>('subject')
+export const SimpleSubjectPredicate = ({
+  sentence,
+  setSentence,
+}: SimpleSubjectPredicateProps) => {
+  const [select, text, reset] = useSelectedText()
+  const [state, setState] = useState<'subject' | 'predicate' | 'final'>(
+    'subject'
+  )
 
-	const [simpleSubject, setSimpleSubject] = useState('')
-	const [simplePredicate, setSimplePredicate] = useState('')
-	const testSentence = 'A good player | respects their team'
-	const newSentence = testSentence.split(' ')
-	const dividedSentence = testSentence.split('|')
+  const [simpleSubject, setSimpleSubject] = useState('')
+  const [simplePredicate, setSimplePredicate] = useState('')
+  const testSentence = 'A good player | respects their team.'
+  const newSentence = testSentence
+    .split(' ')
+    .slice(0, testSentence.split(' ').length)
 
-	const subjectGrader = simpleSubjectGrader({
-		correctSimpleSubject: 'player',
-		givenSimpleSubject: simpleSubject,
-		completeSubject: dividedSentence[0],
-	})
+  const dividedSentence = testSentence.split('|')
 
-	const predicateGrader = simplePredicateGrader({
-		correctSimplePredicate: 'respects',
-		givenSimplePredicate: simplePredicate,
-		completePredicate: dividedSentence[1],
-	})
+  const period = newSentence[newSentence.length - 1].split('')[
+    newSentence[newSentence.length - 1].split('').length - 1
+  ]
+  const lastWordWithOurPeriod = newSentence[newSentence.length - 1]
+    .split('')
+    .slice(0, newSentence[newSentence.length - 1].split('').length - 1)
+    .join('')
 
-	// useEffect(() => {
-	// 	subjectGrader.correctSimpleSubject && setState('predicate')
-	// }, [subjectGrader])
+  const modifiedNewSentece = [...newSentence, lastWordWithOurPeriod, period]
+  console.log(newSentence)
+  const subjectGrader = simpleSubjectGrader({
+    correctSimpleSubject: 'player',
+    givenSimpleSubject: simpleSubject,
+    completeSubject: dividedSentence[0],
+  })
 
-	return (
-		<div>
-			{state === 'subject' && (
-				<div>Find the simple subject of the sentence. Double Click to select the word.</div>
-			)}
-			{state === 'predicate' && (
-				<div>Find the simple predicate of the sentence. Double Click to select the word.</div>
-			)}
-			<br />
-			<div onMouseUp={select}>
-				{newSentence.map((word, i: number) => (
-					<span key={i}>
-						<span
-							onDoubleClick={
-								state === 'subject' && !simpleSubject
-									? () => {
-											setSimpleSubject(text)
-											reset()
-									  }
-									: state === 'subject' && simpleSubject
-									? () => {
-											setSimpleSubject('')
-											reset()
-									  }
-									: () => {
-											setSimplePredicate(text)
-											reset()
-									  }
-							}
-							style={
-								word === simpleSubject
-									? { textDecoration: 'underline' }
-									: word === simplePredicate
-									? {
-											textDecoration: 'underline',
-											textDecorationStyle: 'double',
-									  }
-									: {}
-							}>
-							{word}
-						</span>
-						<span> </span>
-					</span>
-				))}
-				{state === 'subject' && simpleSubject && (
-					<>
-						{!subjectGrader.correctSimpleSubject ? (
-							<div>
-								<div>Incorrect</div>
-								<div>What went wrong? {subjectGrader.whatWentWrong}</div>
-								<div>How to fix? {subjectGrader.howToFix}</div>
-							</div>
-						) : (
-							<div>
-								<div>Correct</div>
-								<div>{subjectGrader.message}</div>
-								<button onClick={() => setState('predicate')}>Next</button>
-							</div>
-						)}
-					</>
-				)}
-			</div>
-		</div>
-	)
+  const predicateGrader = simplePredicateGrader({
+    correctSimplePredicate: 'respects',
+    givenSimplePredicate: simplePredicate,
+    completePredicate: dividedSentence[1],
+  })
+
+  // useEffect(() => {
+  // 	subjectGrader.correctSimpleSubject && setState('predicate')
+  // }, [subjectGrader])
+  const handleDoubleClick = () =>
+    state === 'subject' && !simpleSubject
+      ? () => {
+          setSimpleSubject(text)
+          reset()
+        }
+      : state === 'subject' && simpleSubject
+      ? () => {
+          setSimpleSubject('')
+          setSimpleSubject(text)
+          reset()
+        }
+      : () => {
+          setSimplePredicate(text)
+          reset()
+        }
+  return (
+    <div>
+      {state === 'subject' && (
+        <div>
+          Find the simple subject of the sentence. Double Click to select the
+          word.
+        </div>
+      )}
+      {state === 'predicate' && (
+        <div>
+          Find the simple predicate of the sentence. Double Click to select the
+          word.
+        </div>
+      )}
+      <br />
+      <div onMouseUp={select}>
+        {newSentence.map((word, i: number) => {
+          return (
+            <span key={i}>
+              <span
+                onDoubleClick={handleDoubleClick()}
+                style={
+                  word === simpleSubject
+                    ? { textDecoration: 'underline' }
+                    : word === simplePredicate
+                    ? {
+                        textDecoration: 'underline',
+                        textDecorationStyle: 'double',
+                      }
+                    : {}
+                }
+              >
+                {word.split('')[word.split('').length] === '.'
+                  ? word.split('').slice(0, word.split('').length)
+                  : word}
+              </span>
+              <span> </span>
+            </span>
+          )
+        })}
+        {state === 'subject' && simpleSubject && (
+          <>
+            {!subjectGrader.correctSimpleSubject ? (
+              <div>
+                <div>Incorrect</div>
+                <div>What went wrong? {subjectGrader.whatWentWrong}</div>
+                <div>How to fix? {subjectGrader.howToFix}</div>
+              </div>
+            ) : (
+              <div>
+                <div>Correct</div>
+                <div>{subjectGrader.message}</div>
+                <button onClick={() => setState('predicate')}>Next</button>
+              </div>
+            )}
+          </>
+        )}
+        {state === 'predicate' && simplePredicate && (
+          <>
+            {!predicateGrader.correctSimplePredicate ? (
+              <div>
+                <div>Incorrect</div>
+                <div>What went wrong? {predicateGrader.whatWentWrong}</div>
+                <div>How to fix? {predicateGrader.howToFix}</div>
+              </div>
+            ) : (
+              <div>
+                <div>Correct</div>
+                <div>{predicateGrader.message}</div>
+                {/* <button onClick={() => setState('predicate')}>Next</button> */}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  )
 }
--- src/components/dashboard/teacher/development/grammar/simplePredicateGrader.ts
+++ src/components/dashboard/teacher/development/grammar/simplePredicateGrader.ts
@@ -1,37 +1,33 @@
 export type SimplePredicateGraderProps = {
-	correctSimplePredicate: string
-	givenSimplePredicate: string
-	completePredicate: string
+  correctSimplePredicate: string
+  givenSimplePredicate: string
+  completePredicate: string
 }
 
 export const simplePredicateGrader = ({
-	correctSimplePredicate,
-	givenSimplePredicate,
-	completePredicate,
+  correctSimplePredicate,
+  givenSimplePredicate,
+  completePredicate,
 }: SimplePredicateGraderProps) => {
-	if (!completePredicate.includes(givenSimplePredicate)) {
-		return {
-			whatWentWrong: 'Your simple predicate is in the subject.',
-			howToFix:
-				'Look at the subject only and find the person, place, idea, or thing that is in the complete subject.',
-			correctSimplePredicate: false,
-		}
-	}
-	if (givenSimplePredicate !== correctSimplePredicate) {
-		return {
-			whatWentWrong:
-				'Your simple predicate is in the complete subject, but is not the verb in the complete predicate',
-			howToFix:
-				'Find the action word, being word, or feeling word that is in the complete predicate.',
-			correctSimplePredicate: false,
-		}
-	}
-	// if (givenSimplePredicate === correctSimplePredicate) {
-	return {
-		correctSimplePredicate: true,
-		message: 'Right again!',
-	}
-	// }
-
-	// return { message: `Everything is good` }
+  if (!completePredicate.includes(givenSimplePredicate)) {
+    return {
+      whatWentWrong: 'Your simple predicate is in the subject.',
+      howToFix:
+        'Look at the subject only and find the person, place, idea, or thing that is in the complete subject.',
+      correctSimplePredicate: false,
+    }
+  }
+  if (givenSimplePredicate !== correctSimplePredicate) {
+    return {
+      whatWentWrong:
+        'Your simple predicate is in the complete predicate, but is not the verb in the complete predicate',
+      howToFix:
+        'Find the action word, being word, or feeling word that is in the complete predicate.',
+      correctSimplePredicate: false,
+    }
+  }
+  return {
+    correctSimplePredicate: true,
+    message: 'Right again!',
+  }
 }
