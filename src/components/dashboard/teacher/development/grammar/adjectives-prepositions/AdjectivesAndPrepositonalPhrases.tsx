977a3a6a7ce444d76c01adf4de21ee629e975302
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-14 06:42:16 -0400

    moved into separate folder

---
+++ src/components/dashboard/teacher/development/grammar/adjectives-prepositions/AdjectivesAndPrepositonalPhrases.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useSelectedText } from '../../../../../../hooks/useSelectedText'
+
+export type AdjectivesAndPrepositonalPhrasesProps = {}
+
+export const AdjectivesAndPrepositonalPhrases =
+  ({}: AdjectivesAndPrepositonalPhrasesProps) => {
+    const [select, text, reset] = useSelectedText()
+    const [state, setState] =
+      useState<
+        | 'subject-adjectives'
+        | 'subject-prepositions'
+        | 'predicate-adjectives'
+        | 'predicate-prepositions'
+      >('subject-adjectives')
+    const testSentence =
+      'A good player with high reputation | respects their team.'
+    const testCompleteSubject = testSentence.split('|')[0]
+    const testCompletePredicate = testSentence.split('|')[1]
+    const adjectiveList: string[] = ['good']
+    const prepostionalPhrasesList: string[] = ['with high reputation']
+    return (
+      <div>
+        <div>
+          Find the adjectives and prepositional phrases that modify the simple
+          subject
+        </div>
+        <div onMouseUp={select}>
+          <span>{testCompleteSubject}</span>
+          <span> | </span>
+          <span>{testCompletePredicate}</span>
+        </div>
+        <div>{text}</div>
+      </div>
+    )
+  }
