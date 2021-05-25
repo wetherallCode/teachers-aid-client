27e51d8c77071c9d8cf0d6e36478c1e639a1fdf3
Rob Wetherall <robertgwetherall1@gmail.com>
2021-05-25 06:30:29 -0400

    added a reset function

--- src/components/dashboard/teacher/development/grammar/question-deconstruction/QuestionWordRemoval.tsx
+++ src/components/dashboard/teacher/development/grammar/question-deconstruction/QuestionWordRemoval.tsx
@@ -4,56 +4,80 @@ import { splitSentenceByWord } from '../../../../../../utils'
 import { QuestionProps } from './QuestionDeconstruction'
 
 export type QuestionWordRemovalProps = {
-	question: QuestionProps
+  question: QuestionProps
 }
 
 export const QuestionWordRemoval = ({ question }: QuestionWordRemovalProps) => {
-	const [select, text, reset] = useSelectedText()
-	const [questionToModify, setQuestionToModify] = useState(splitSentenceByWord(question.original))
-	const [deletedWords, setDeletedWords] = useState<string[]>([])
+  const [select, text, reset] = useSelectedText()
+  const [questionToModify, setQuestionToModify] = useState(
+    splitSentenceByWord(question.original)
+  )
+  const [deletedWords, setDeletedWords] = useState<string[]>([])
 
-	// const handleWordRemoval  = (word: string, )
-	return (
-		<div>
-			<div onMouseUp={select}>
-				{questionToModify.map((word, i: number) => (
-					<span key={i}>
-						<span
-							onDoubleClick={() => {
-								const index = questionToModify.findIndex((wordToFind) => wordToFind === word)
-								setQuestionToModify([
-									...questionToModify.slice(0, index),
-									...questionToModify.slice(index + 1),
-								])
-								setDeletedWords((list) => [...list, word])
-							}}>
-							{word}
-						</span>
-						{word !== questionToModify[questionToModify.length - 2] && <span> </span>}
-					</span>
-				))}
-			</div>
+  // const handleWordRemoval  = (word: string, )
+  return (
+    <div>
+      <div onMouseUp={select}>
+        {questionToModify.map((word, i: number) => (
+          <span key={i}>
+            <span
+              onDoubleClick={() => {
+                const index = questionToModify.findIndex(
+                  (wordToFind) => wordToFind === word
+                )
+                setQuestionToModify([
+                  ...questionToModify.slice(0, index),
+                  ...questionToModify.slice(index + 1),
+                ])
+                setDeletedWords((list) => [...list, word])
+              }}
+            >
+              {word}
+            </span>
+            {word !== questionToModify[questionToModify.length - 2] && (
+              <span> </span>
+            )}
+          </span>
+        ))}
+      </div>
 
-			<br />
-			<div>Deleted Words</div>
-			<div onMouseUp={select}>
-				{deletedWords.map((word, i: number) => (
-					<span key={i}>
-						<span
-							onDoubleClick={() => {
-								const index = splitSentenceByWord(question.original).findIndex(
-									(wordToFind) => wordToFind === word
-								)
-								setQuestionToModify(() => {
-									if (true) return [word]
-								})
-							}}>
-							{word}
-						</span>
-						<span> </span>
-					</span>
-				))}
-			</div>
-		</div>
-	)
+      <br />
+      <div>Deleted Words</div>
+      <div>
+        <button
+          onClick={() => {
+            setQuestionToModify(question.original.split(' '))
+            setDeletedWords([])
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div onMouseUp={select}>
+        {deletedWords.map((word, i: number) => (
+          <span key={i}>
+            <span
+              onDoubleClick={() => {
+                const index = splitSentenceByWord(question.original).findIndex(
+                  (wordToFind) => wordToFind === word
+                )
+
+                const returnedWords = question.original
+                  .split(' ')
+                  .filter(
+                    (wordToFilter) =>
+                      wordToFilter === word ||
+                      questionToModify.includes(wordToFilter)
+                  )
+                console.log(returnedWords)
+              }}
+            >
+              {word}
+            </span>
+            <span> </span>
+          </span>
+        ))}
+      </div>
+    </div>
+  )
 }
