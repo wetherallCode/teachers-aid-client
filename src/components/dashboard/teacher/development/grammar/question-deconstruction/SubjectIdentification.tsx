50f70b557304adfa97e9637b479dfff1363de020
Rob Wetherall <robertgwetherall1@gmail.com>
2021-06-08 07:31:20 -0400

    more work

--- src/components/dashboard/teacher/development/grammar/question-deconstruction/SubjectIdentification.tsx
+++ src/components/dashboard/teacher/development/grammar/question-deconstruction/SubjectIdentification.tsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import { QuestionProps } from './QuestionDeconstruction'
 
 export type SubjectIdentificationProps = {
-  setState: React.Dispatch<
-    React.SetStateAction<
-      | 'question-word-removal'
-      | 'helping-verb-id'
-      | 'subject-predicate-split'
-      | 'subject-identification'
-      | 'verb-identification'
-      | 'object-identification'
-    >
-  >
-  questionToModify: string[]
-  question: QuestionProps
+	setState: React.Dispatch<
+		React.SetStateAction<
+			| 'question-word-removal'
+			| 'helping-verb-id'
+			| 'subject-predicate-split'
+			| 'subject-identification'
+			| 'verb-identification'
+			| 'object-identification'
+		>
+	>
+	questionToModify: string[]
+	question: QuestionProps
+	subject: string | null
+	setSubject: Dispatch<SetStateAction<string | null>>
 }
 
 export const SubjectIdentification = ({
-  setState,
-  question,
-  questionToModify,
+	setState,
+	question,
+	questionToModify,
+	subject,
+	setSubject,
 }: SubjectIdentificationProps) => {
-  return (
-    <>
-      <div>{questionToModify.join(' ')}</div>
-    </>
-  )
+	return (
+		<>
+			Select the Complete Subject of the question
+			<div>{questionToModify.join(' ')}</div>
+		</>
+	)
 }
