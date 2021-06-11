fc20c9b77331fd343ccb49fae4e330df8a2417ef
Rob Wetherall <robertgwetherall1@gmail.com>
2021-06-11 15:07:31 -0400

    added comment

--- src/hooks/useSelectedText.ts
+++ src/hooks/useSelectedText.ts
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 export const useSelectedText = () => {
-  const [text, setText] = useState('')
+	const [text, setText] = useState('')
+	// make text null at init
 
-  const select = () => {
-    const selected = window.getSelection() as Selection
-    setText(selected.toString())
-  }
-  const reset = () => setText('')
-  return [select, text, reset] as const
+	const select = () => {
+		const selected = window.getSelection() as Selection
+		setText(selected.toString())
+	}
+	const reset = () => setText('')
+	return [select, text, reset] as const
 }
