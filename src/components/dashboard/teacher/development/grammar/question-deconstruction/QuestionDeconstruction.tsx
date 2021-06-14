66e1f43e4a46aab9a9b52feb9bc9e2cbcd6d720a
Rob Wetherall <robertgwetherall1@gmail.com>
2021-06-14 13:17:26 -0400

    added LineToManipulate styles

--- src/components/dashboard/teacher/development/grammar/question-deconstruction/QuestionDeconstruction.tsx
+++ src/components/dashboard/teacher/development/grammar/question-deconstruction/QuestionDeconstruction.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { splitSentenceByWord } from '../../../../../../utils'
 import { HelpingVerbIdentification } from './HelpingVerbIdentification'
+import { ObjectIdentification } from './ObjectIdentification'
 import { QuestionWordRemoval } from './QuestionWordRemoval'
 import { SubjectIdentification } from './SubjectIdentification'
 import { SubjectPredicateSplit } from './SubjectPredicateSplit'
@@ -9,114 +10,123 @@ import { VerbIdentification } from './VerbIdentification'
 export type QuestionDeconstructionProps = {}
 
 export type QuestionProps = {
-  original: string
-  modifiedQuestion: string
-  questionWord: string
-  helpingVerb: string
-  completeSubject: string
-  simpleSubject: string
-  nounType: 'PERSON' | 'PLACE' | 'THING' | 'IDEA'
-  verbType: 'ACTION' | 'BEING' | 'FEELING'
-  compoundNoun: boolean
-  completePredicate: string
-  simplePredicate: string
+	original: string
+	modifiedQuestion: string
+	questionWord: string
+	helpingVerb: string
+	completeSubject: string
+	simpleSubject: string
+	nounType: 'PERSON' | 'PLACE' | 'THING' | 'IDEA'
+	verbType: 'ACTION' | 'BEING' | 'FEELING'
+	compoundNoun: boolean
+	completePredicate: string
+	simplePredicate: string
+	object: null | string
 }
 
 export type QuestionDeconstructProps =
-  | 'question-word-removal'
-  | 'subject-predicate-split'
-  | 'helping-verb-id'
-  | 'subject-identification'
-  | 'verb-identification'
-  | 'object-identification'
+	| 'question-word-removal'
+	| 'subject-predicate-split'
+	| 'helping-verb-id'
+	| 'subject-identification'
+	| 'verb-identification'
+	| 'object-identification'
 
 export const QuestionDeconstruction = ({}: QuestionDeconstructionProps) => {
-  const whyWasQuestion: QuestionProps = {
-    original: `Why was John Brown executed for treason?`,
-    modifiedQuestion: `Why was John Brown executed for treason?`,
-    questionWord: 'why',
-    helpingVerb: 'was',
-    completeSubject: 'John Brown',
-    simpleSubject: 'John Brown',
-    nounType: 'PERSON',
-    verbType: 'ACTION',
-    compoundNoun: true,
-    completePredicate: 'executed for treason?',
-    simplePredicate: 'executed',
-  }
+	const whyWasQuestion: QuestionProps = {
+		original: `Why was John Brown executed for treason?`,
+		modifiedQuestion: `Why was John Brown executed for treason?`,
+		questionWord: 'why',
+		helpingVerb: 'was',
+		completeSubject: 'John Brown',
+		simpleSubject: 'John Brown',
+		nounType: 'PERSON',
+		verbType: 'ACTION',
+		compoundNoun: true,
+		completePredicate: 'executed for treason?',
+		simplePredicate: 'executed',
+		object: null,
+	}
 
-  const howDidQuestion: QuestionProps = {
-    original: `How did John Brown affect the nation?`,
-    modifiedQuestion: `How did John Brown affect the nation?`,
-    questionWord: 'how',
-    helpingVerb: 'did',
-    completeSubject: 'John Brown',
-    simpleSubject: 'John Brown',
-    nounType: 'PERSON',
-    verbType: 'ACTION',
-    compoundNoun: true,
-    completePredicate: 'affect the nation',
-    simplePredicate: 'affect',
-  }
+	const howDidQuestion: QuestionProps = {
+		original: `How did John Brown affect the nation?`,
+		modifiedQuestion: `How did John Brown affect the nation?`,
+		questionWord: 'how',
+		helpingVerb: 'did',
+		completeSubject: 'John Brown',
+		simpleSubject: 'John Brown',
+		nounType: 'PERSON',
+		verbType: 'ACTION',
+		compoundNoun: true,
+		completePredicate: 'affect the nation',
+		simplePredicate: 'affect',
+		object: 'the nation',
+	}
 
-  // const question = howDidQuestion
-  const question = whyWasQuestion
-  const [questionToModify, setQuestionToModify] = useState(
-    splitSentenceByWord(question.original)
-  )
-  const [subject, setSubject] = useState<string | null>(null)
-  const [verb, setVerb] = useState<string | null>(null)
+	// const question = howDidQuestion
+	const question = whyWasQuestion
+	const [questionToModify, setQuestionToModify] = useState(splitSentenceByWord(question.original))
+	const [subject, setSubject] = useState<string | null>(null)
+	const [verb, setVerb] = useState<string | null>(null)
+	const [object, setObject] = useState<string | null>(null)
 
-  const [state, setState] = useState<QuestionDeconstructProps>(
-    'question-word-removal'
-  )
+	const [state, setState] = useState<QuestionDeconstructProps>('question-word-removal')
 
-  return (
-    <div>
-      <div>Deconstruct the Question</div>
+	return (
+		<div>
+			<div>Deconstruct the Question</div>
 
-      {state === 'question-word-removal' && (
-        <QuestionWordRemoval
-          question={question}
-          questionToModify={questionToModify}
-          setQuestionToModify={setQuestionToModify}
-          setState={setState}
-        />
-      )}
-      {state === 'subject-predicate-split' && (
-        <SubjectPredicateSplit
-          questionToModify={questionToModify}
-          setQuestionToModify={setQuestionToModify}
-          setState={setState}
-          question={question}
-        />
-      )}
-      {state === 'helping-verb-id' && (
-        <HelpingVerbIdentification
-          questionToModify={questionToModify}
-          setQuestionToModify={setQuestionToModify}
-          setState={setState}
-          question={question}
-        />
-      )}
-      {state === 'subject-identification' && (
-        <SubjectIdentification
-          setState={setState}
-          questionToModify={questionToModify}
-          question={question}
-          subject={subject}
-          setSubject={setSubject}
-        />
-      )}
-      {state === 'verb-identification' && (
-        <VerbIdentification
-          setState={setState}
-          questionToModify={questionToModify}
-          question={question}
-          verb={verb}
-          setVerb={setVerb}
-        />
-      )}
-    </div>
-  )
+			{state === 'question-word-removal' && (
+				<QuestionWordRemoval
+					question={question}
+					questionToModify={questionToModify}
+					setQuestionToModify={setQuestionToModify}
+					setState={setState}
+				/>
+			)}
+			{state === 'subject-predicate-split' && (
+				<SubjectPredicateSplit
+					questionToModify={questionToModify}
+					setQuestionToModify={setQuestionToModify}
+					setState={setState}
+					question={question}
+				/>
+			)}
+			{state === 'helping-verb-id' && (
+				<HelpingVerbIdentification
+					questionToModify={questionToModify}
+					setQuestionToModify={setQuestionToModify}
+					setState={setState}
+					question={question}
+				/>
+			)}
+			{state === 'subject-identification' && (
+				<SubjectIdentification
+					setState={setState}
+					questionToModify={questionToModify}
+					question={question}
+					subject={subject}
+					setSubject={setSubject}
+				/>
+			)}
+			{state === 'verb-identification' && (
+				<VerbIdentification
+					setState={setState}
+					questionToModify={questionToModify}
+					question={question}
+					verb={verb}
+					setVerb={setVerb}
+				/>
+			)}
+			{state === 'object-identification' && (
+				<ObjectIdentification
+					setState={setState}
+					questionToModify={questionToModify}
+					question={question}
+					object={object}
+					setObject={setVerb}
+				/>
+			)}
+		</div>
+	)
 }
--- src/components/dashboard/teacher/development/grammar/question-deconstruction/SubjectIdentification.tsx
+++ src/components/dashboard/teacher/development/grammar/question-deconstruction/SubjectIdentification.tsx
@@ -1,88 +1,82 @@
 import React, { Dispatch, SetStateAction, useEffect } from 'react'
 import { useSelectedText } from '../../../../../../hooks/useSelectedText'
 import { simpleSubjectGrader } from '../simple-subject-predicate/simpleSubjectGrader'
-import {
-  QuestionProps,
-  QuestionDeconstructProps,
-} from './QuestionDeconstruction'
+import { QuestionProps, QuestionDeconstructProps } from './QuestionDeconstruction'
+import { LineToManipulate } from './VerbIdentification'
 
 export type SubjectIdentificationProps = {
-  setState: Dispatch<SetStateAction<QuestionDeconstructProps>>
-  questionToModify: string[]
-  question: QuestionProps
-  subject: string | null
-  setSubject: Dispatch<SetStateAction<string | null>>
+	setState: Dispatch<SetStateAction<QuestionDeconstructProps>>
+	questionToModify: string[]
+	question: QuestionProps
+	subject: string | null
+	setSubject: Dispatch<SetStateAction<string | null>>
 }
 
 export const SubjectIdentification = ({
-  setState,
-  question,
-  questionToModify,
-  subject,
-  setSubject,
+	setState,
+	question,
+	questionToModify,
+	subject,
+	setSubject,
 }: SubjectIdentificationProps) => {
-  const [select, text, reset] = useSelectedText()
-  const isAnswered = text === ''
+	const [select, text, reset] = useSelectedText()
+	const isAnswered = text === ''
 
-  const { correctSimpleSubject, howToFix, message, whatWentWrong } =
-    simpleSubjectGrader({
-      correctSimpleSubject: question.simpleSubject,
-      givenSimpleSubject: text.trim(),
-      completeSubject: question.completeSubject,
-    })
+	const { correctSimpleSubject, howToFix, message, whatWentWrong } = simpleSubjectGrader({
+		correctSimpleSubject: question.simpleSubject,
+		givenSimpleSubject: text.trim(),
+		completeSubject: question.completeSubject,
+	})
 
-  useEffect(() => {
-    if (correctSimpleSubject) {
-      setSubject(text)
-      setTimeout(() => {
-        setState('verb-identification')
-      }, 3000)
-    }
-  }, [correctSimpleSubject])
+	useEffect(() => {
+		if (correctSimpleSubject) {
+			setSubject(text)
+			setTimeout(() => {
+				setState('verb-identification')
+			}, 3000)
+		}
+	}, [correctSimpleSubject])
 
-  return (
-    <>
-      <div>Select the Simple Subject of the question</div>
-      <div>
-        Simple subjects are the noun in the complete subect. There may be
-        adjectives that modify the noun or prepositional phrases that add
-        specificity to the noun.
-      </div>
+	return (
+		<>
+			<div>Select the Simple Subject of the question</div>
+			<div>
+				Simple subjects are the noun in the complete subect. There may be adjectives that modify the
+				noun or prepositional phrases that add specificity to the noun.
+			</div>
 
-      <div onMouseUp={select}>
-        {questionToModify
-          .join(' ')
-          .split(' ')
-          .map((part, i: number) => (
-            <span key={i}>
-              <span>{part}</span>
-              {part !== questionToModify[questionToModify.length - 1] && (
-                <span> </span>
-              )}
-            </span>
-          ))}
-      </div>
-      <div>
-        {isAnswered ? (
-          ''
-        ) : (
-          <div>
-            <div>{correctSimpleSubject ? 'Correct' : 'Not Quite'}</div>
-            {!correctSimpleSubject ? (
-              <>
-                <div>
-                  What went wrong? <span>{whatWentWrong}</span>
-                </div>
-                <div>
-                  How do you fix it? <span>{howToFix}</span>
-                </div>
-              </>
-            ) : (
-              <div>{message}</div>
-            )}
-          </div>
-        )}
-      </div>
-    </>
-  )
+			<LineToManipulate onMouseUp={select}>
+				{questionToModify
+					.join(' ')
+					.split(' ')
+					.map((part, i: number) => (
+						<span key={i}>
+							<span>{part}</span>
+							{part !== questionToModify[questionToModify.length - 1] && <span> </span>}
+						</span>
+					))}
+			</LineToManipulate>
+			<div>
+				{isAnswered ? (
+					''
+				) : (
+					<div>
+						<div>{correctSimpleSubject ? 'Correct' : 'Not Quite'}</div>
+						{!correctSimpleSubject ? (
+							<>
+								<div>
+									What went wrong? <span>{whatWentWrong}</span>
+								</div>
+								<div>
+									How do you fix it? <span>{howToFix}</span>
+								</div>
+							</>
+						) : (
+							<div>{message}</div>
+						)}
+					</div>
+				)}
+			</div>
+		</>
+	)
 }
