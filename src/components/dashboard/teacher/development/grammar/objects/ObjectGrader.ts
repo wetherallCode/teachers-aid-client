fb3ffc10b1c42c9a6c86e154a03e89e96c37977b
Rob Wetherall <robertgwetherall1@gmail.com>
2021-06-17 06:37:31 -0400

    more work on objects

--- src/components/dashboard/teacher/development/grammar/objects/ObjectGrader.ts
+++ src/components/dashboard/teacher/development/grammar/objects/ObjectGrader.ts
@@ -2,67 +2,85 @@ import { irregularPastTenseVerbList } from '../../../../../../utils'
 import { findPreposition } from '../simple-subject-predicate/findPreposition'
 
 type ObjectGraderProps = {
-	givenObject: string
-	correctObject: string
-	completePredicate: string
-	completeSubject: string
-	simplePredicate: string
+  givenObject: string
+  correctObject: string
+  completePredicate: string
+  completeSubject: string
+  simplePredicate: string
 }
 
 export function objectGrader({
-	givenObject,
-	correctObject,
-	completePredicate,
-	completeSubject,
-	simplePredicate,
+  givenObject,
+  correctObject,
+  completePredicate,
+  completeSubject,
+  simplePredicate,
 }: ObjectGraderProps) {
-	const conjugatedVerb =
-		irregularPastTenseVerbList(simplePredicate) === simplePredicate
-			? simplePredicate + 'ed'
-			: irregularPastTenseVerbList(simplePredicate)
-	const nonContextSentence = completeSubject + ' ' + conjugatedVerb + ' ' + correctObject
+  const conjugatedVerb =
+    irregularPastTenseVerbList(simplePredicate) === simplePredicate
+      ? simplePredicate + 'ed'
+      : irregularPastTenseVerbList(simplePredicate)
+  const nonContextSentence =
+    completeSubject + ' ' + conjugatedVerb + ' ' + correctObject
 
-	if (givenObject !== '' && completeSubject.includes(givenObject)) {
-		return {
-			correctObject: false,
-			message: 'Your object must be in the predicate.',
-			howToFix: `Ask yourself: Did ${completeSubject}
+  console.log(givenObject === correctObject)
+  if (givenObject !== '' && completeSubject.includes(givenObject)) {
+    return {
+      correctObject: false,
+      message: 'Your object must be in the predicate.',
+      howToFix: `Ask yourself: Did ${completeSubject}
       ${simplePredicate} something?`,
-		}
-	}
-	if (givenObject !== '' && givenObject.includes(simplePredicate)) {
-		return {
-			correctObject: false,
-			message: `Your object must come after the verb: ${simplePredicate}`,
-			howToFix: `Ask yourself: Did ${completeSubject}
+    }
+  }
+  if (givenObject !== '' && givenObject.includes(simplePredicate)) {
+    return {
+      correctObject: false,
+      message: `Your object must come after the verb: ${simplePredicate}`,
+      howToFix: `Ask yourself: Did ${completeSubject}
       ${simplePredicate} something?`,
-		}
-	}
-	if (givenObject !== '' && findPreposition(givenObject)) {
-		return {
-			correctObject: false,
-			message: 'Your object cannot include a preposition.',
-			howToFix: `Ask yourself: Did ${completeSubject}
+    }
+  }
+  if (givenObject !== '' && findPreposition(givenObject)) {
+    return {
+      correctObject: false,
+      message: 'Your object cannot include a preposition.',
+      howToFix: `Ask yourself: Did ${completeSubject}
       ${simplePredicate} something?`,
-		}
-	}
-	if (givenObject !== '' && !nonContextSentence.includes(givenObject)) {
-		return {
-			correctObject: false,
-			message: 'Your selection is not the word that comes right after the verb.',
-			howToFix: `The Object of the verb is going to come directly after the verb.`,
-		}
-	}
-	if (givenObject !== '' && !findPreposition(givenObject) && givenObject !== correctObject) {
-		// if the given is one word but the correct is more than one word
-		// if the given is more than one word, but the correct is one word
-
-		return {
-			correctObject: false,
-			message: 'Your selection is not the object. Please try again.',
-			howToFix: `Ask yourself: Did ${completeSubject}
+    }
+  }
+  if (givenObject !== '' && !nonContextSentence.includes(givenObject)) {
+    return {
+      correctObject: false,
+      message:
+        'Your selection is not the word that comes right after the verb.',
+      howToFix: `The Object of the verb is going to come directly after the verb.`,
+    }
+  }
+  if (
+    givenObject !== '' &&
+    // !findPreposition(givenObject) &&
+    givenObject !== correctObject
+  ) {
+    // if the given is one word but the correct is more than one word
+    if (
+      givenObject.split(' ').length === 1 &&
+      correctObject.split(' ').length > 1
+    ) {
+      console.log('given is one word but the correct is more than one word')
+    }
+    // if the given is more than one word, but the correct is one word
+    if (
+      givenObject.split(' ').length > 1 &&
+      correctObject.split(' ').length === 1
+    ) {
+      console.log('given is more than one word, but the correct is one word')
+    }
+    return {
+      correctObject: false,
+      message: 'Your selection is not the object. Please try again.',
+      howToFix: `Ask yourself: Did ${completeSubject}
       ${simplePredicate} something?`,
-		}
-	}
-	return { correctObject: true, correctMessage: 'Good Job' }
+    }
+  }
+  return { correctObject: true, correctMessage: 'Good Job' }
 }
