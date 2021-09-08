808a6153b9907782e3f4111fa845795a9d521f29
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-08 08:19:31 -0400

    formatted

--- src/components/dashboard/teacher/teachers-aid/class-control-panel/protocol-manager/ProtocolManager.tsx
+++ src/components/dashboard/teacher/teachers-aid/class-control-panel/protocol-manager/ProtocolManager.tsx
@@ -1,60 +1,54 @@
 import React, { FC, useEffect } from 'react'
 import {
-  findLessonByCourseAndDate_findLessonByCourseAndDate_lesson_duringActivities,
-  findLessonByCourseAndDate_findLessonByCourseAndDate_lesson,
+	findLessonByCourseAndDate_findLessonByCourseAndDate_lesson_duringActivities,
+	findLessonByCourseAndDate_findLessonByCourseAndDate_lesson,
 } from '../../../../../../schemaTypes'
 import { useTeachersAidContextProvider } from '../../state/TeachersAidContext'
 import { ActivatedProtocolDisplay } from './ActivatedProtocolDisplay'
 import { ProtocolSelector } from './ProtocolSelector'
 
 export type ProtocolManagerProps = {
-  lesson: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson
-  protocols: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson_duringActivities[]
-  presentStudentList: string[]
+	lesson: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson
+	protocols: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson_duringActivities[]
+	presentStudentList: string[]
 }
 
 export const ProtocolManager: FC<ProtocolManagerProps> = ({
-  lesson,
-  protocols,
-  presentStudentList,
+	lesson,
+	protocols,
+	presentStudentList,
 }) => {
-  const [state, event] = useTeachersAidContextProvider()
+	const [state, event] = useTeachersAidContextProvider()
 
-  const currentActiveProtocolIndex = protocols.findIndex(
-    (protocol) => protocol.isActive
-  )
+	const currentActiveProtocolIndex = protocols.findIndex((protocol) => protocol.isActive)
 
-  useEffect(() => {
-    event({
-      type: 'SELECT_PROTOCOL',
-      payload:
-        currentActiveProtocolIndex === -1 ? 0 : currentActiveProtocolIndex,
-    })
-  }, [currentActiveProtocolIndex, event])
+	useEffect(() => {
+		event({
+			type: 'SELECT_PROTOCOL',
+			payload: currentActiveProtocolIndex === -1 ? 0 : currentActiveProtocolIndex,
+		})
+	}, [currentActiveProtocolIndex, event])
 
-  useEffect(() => {
-    event({
-      type: 'UPDATE_LESSON_PROTOCOL',
-      payload: protocols[state.context.protocolSelect],
-    })
-  }, [event, protocols, state.context.protocolSelect])
-
-  return (
-    <>
-      {lesson && protocols ? (
-        <>
-          {!protocols.some((protocol) => protocol.isActive) ? (
-            <ProtocolSelector
-              lesson={lesson}
-              presentStudentList={presentStudentList}
-            />
-          ) : (
-            <ActivatedProtocolDisplay lessonId={lesson._id!} />
-          )}
-        </>
-      ) : (
-        <div>No Lesson Scheduled for Today</div>
-      )}
-    </>
-  )
+	useEffect(() => {
+		event({
+			type: 'UPDATE_LESSON_PROTOCOL',
+			payload: protocols[state.context.protocolSelect],
+		})
+	}, [event, protocols, state.context.protocolSelect])
+	console.log(protocols)
+	return (
+		<>
+			{lesson && protocols ? (
+				<>
+					{!protocols.some((protocol) => protocol.isActive) ? (
+						<ProtocolSelector lesson={lesson} presentStudentList={presentStudentList} />
+					) : (
+						<ActivatedProtocolDisplay lessonId={lesson._id!} />
+					)}
+				</>
+			) : (
+				<div>No Lesson Scheduled for Today</div>
+			)}
+		</>
+	)
 }
