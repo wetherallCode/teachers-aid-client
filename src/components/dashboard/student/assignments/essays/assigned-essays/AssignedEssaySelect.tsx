f9b8bbd1c885dcaf5a93f10e2c8ea324ab472dff
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-17 14:52:25 -0400

    completed essays should no longer show up

--- src/components/dashboard/student/assignments/essays/assigned-essays/AssignedEssaySelect.tsx
+++ src/components/dashboard/student/assignments/essays/assigned-essays/AssignedEssaySelect.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react'
 import { gql, useQuery } from '@apollo/client'
 import {
-  findEssaysToComplete,
-  findEssaysToCompleteVariables,
-  me_me_Student,
+	findEssaysToComplete,
+	findEssaysToCompleteVariables,
+	me_me_Student,
 } from '../../../../../../schemaTypes'
 import { useUserContextProvider } from '../../../../../../contexts/UserContext'
 import { Link } from 'react-router-dom'
 import {
-  AssignmentTypeTitle,
-  AssignmentTypeContentContainer,
-  AssignmentLink,
-  CompletionMessage,
-  AssignmentLinkLi,
+	AssignmentTypeTitle,
+	AssignmentTypeContentContainer,
+	AssignmentLink,
+	CompletionMessage,
+	AssignmentLinkLi,
 } from '../../state-n-styles/assignmentsStyles'
 
 import { useStudentAssignmentContextProvider } from '../../state-n-styles/StudentAssignmentContext'
@@ -20,102 +20,99 @@ import { useStudentAssignmentContextProvider } from '../../state-n-styles/Studen
 export type AssignedEssaySelectProps = {}
 
 export const ESSAYS_TO_COMPLETE_QUERY = gql`
-  query findEssaysToComplete($input: FindEssaysToCompleteByStudentIdInput!) {
-    findEssaysToCompleteByStudentId(input: $input) {
-      essays {
-        _id
-        paperBased
-        assigned
-        missing
-        readings {
-          readingSections
-        }
-        topic {
-          writingLevel
-        }
-        markingPeriod
-      }
-    }
-  }
+	query findEssaysToComplete($input: FindEssaysToCompleteByStudentIdInput!) {
+		findEssaysToCompleteByStudentId(input: $input) {
+			essays {
+				_id
+				paperBased
+				assigned
+				missing
+				readings {
+					readingSections
+				}
+				topic {
+					writingLevel
+				}
+				markingPeriod
+			}
+		}
+	}
 `
 
 export const AssignedEssaySelect: FC<AssignedEssaySelectProps> = () => {
-  const me: me_me_Student = useUserContextProvider()
-  const [state] = useStudentAssignmentContextProvider()
+	const me: me_me_Student = useUserContextProvider()
+	const [state] = useStudentAssignmentContextProvider()
 
-  const { loading, data } = useQuery<
-    findEssaysToComplete,
-    findEssaysToCompleteVariables
-  >(ESSAYS_TO_COMPLETE_QUERY, {
-    variables: {
-      input: { studentId: me._id! },
-    },
-    pollInterval: 1000,
-    onCompleted: (data) => console.log(data),
-    onError: (error) => console.error(error),
-  })
+	const { loading, data } = useQuery<findEssaysToComplete, findEssaysToCompleteVariables>(
+		ESSAYS_TO_COMPLETE_QUERY,
+		{
+			variables: {
+				input: { studentId: me._id! },
+			},
+			pollInterval: 1000,
+			onCompleted: (data) => console.log(data),
+			onError: (error) => console.error(error),
+		}
+	)
 
-  const essaysForMarkingPeriod =
-    data?.findEssaysToCompleteByStudentId.essays.filter(
-      (essay) => essay.markingPeriod === state.context.selectedMarkingPeriod
-    )!
-  const allEssaysComplete =
-    essaysForMarkingPeriod &&
-    essaysForMarkingPeriod.length > 0 &&
-    essaysForMarkingPeriod.every((essay) => !essay.missing && !essay.assigned)
+	const essaysForMarkingPeriod = data?.findEssaysToCompleteByStudentId.essays.filter(
+		(essay) => essay.markingPeriod === state.context.selectedMarkingPeriod
+	)!
+	const allEssaysComplete =
+		essaysForMarkingPeriod &&
+		essaysForMarkingPeriod.length > 0 &&
+		essaysForMarkingPeriod.every((essay) => !essay.missing && !essay.assigned)
 
-  const noEssaysHaveBeenAssigned =
-    (essaysForMarkingPeriod &&
-      essaysForMarkingPeriod.every(
-        (essay) => essay.missing && !essay.assigned
-      )) ||
-    (essaysForMarkingPeriod && essaysForMarkingPeriod.length === 0)
-
-  return (
-    <>
-      <AssignmentTypeTitle>
-        <div>Essays to Complete</div>
-      </AssignmentTypeTitle>
-      {loading ? null : (
-        <>
-          {allEssaysComplete ? (
-            <AssignmentTypeContentContainer>
-              <CompletionMessage>
-                <ul>
-                  <li>All Essays for Marking Period Complete</li>
-                </ul>
-              </CompletionMessage>
-            </AssignmentTypeContentContainer>
-          ) : noEssaysHaveBeenAssigned ? (
-            <AssignmentTypeContentContainer>
-              <CompletionMessage>
-                <ul>
-                  <li>No Essays Assigned</li>
-                </ul>
-              </CompletionMessage>
-            </AssignmentTypeContentContainer>
-          ) : (
-            <>
-              {essaysForMarkingPeriod && (
-                <AssignmentTypeContentContainer>
-                  {essaysForMarkingPeriod &&
-                    essaysForMarkingPeriod
-                      .filter((essay) => !essay.paperBased)
-                      .map((essay) => (
-                        <ul key={essay._id}>
-                          <AssignmentLinkLi>
-                            <AssignmentLink
-                              to={`essay/toComplete/${essay._id!}`}
-                              // key={essay._id!}
-                            >
-                              {essay.readings.readingSections}
-                            </AssignmentLink>
-                          </AssignmentLinkLi>
-                        </ul>
-                      ))}
-                </AssignmentTypeContentContainer>
-              )}
-              {/* {essaysForMarkingPeriod && essaysForMarkingPeriod.length === 0 && (
+	const noEssaysHaveBeenAssigned =
+		(essaysForMarkingPeriod &&
+			essaysForMarkingPeriod.every((essay) => essay.missing && !essay.assigned)) ||
+		(essaysForMarkingPeriod && essaysForMarkingPeriod.length === 0)
+	console.log(essaysForMarkingPeriod)
+	return (
+		<>
+			<AssignmentTypeTitle>
+				<div>Essays to Complete</div>
+			</AssignmentTypeTitle>
+			{loading ? null : (
+				<>
+					{allEssaysComplete ? (
+						<AssignmentTypeContentContainer>
+							<CompletionMessage>
+								<ul>
+									<li>All Essays for Marking Period Complete</li>
+								</ul>
+							</CompletionMessage>
+						</AssignmentTypeContentContainer>
+					) : noEssaysHaveBeenAssigned ? (
+						<AssignmentTypeContentContainer>
+							<CompletionMessage>
+								<ul>
+									<li>No Essays Assigned</li>
+								</ul>
+							</CompletionMessage>
+						</AssignmentTypeContentContainer>
+					) : (
+						<>
+							{essaysForMarkingPeriod && (
+								<AssignmentTypeContentContainer>
+									{essaysForMarkingPeriod &&
+										essaysForMarkingPeriod
+											.filter((essay) => !essay.paperBased && essay.assigned)
+											.map((essay) => (
+												<ul key={essay._id}>
+													<AssignmentLinkLi>
+														<AssignmentLink
+															to={`essay/toComplete/${essay._id!}`}
+															// key={essay._id!}
+														>
+															{essay.readings.readingSections}
+														</AssignmentLink>
+													</AssignmentLinkLi>
+												</ul>
+											))}
+								</AssignmentTypeContentContainer>
+							)}
+							{/* {essaysForMarkingPeriod && essaysForMarkingPeriod.length === 0 && (
                 <AssignmentTypeContentContainer>
                   <CompletionMessage>
                     <ul>
@@ -124,10 +121,10 @@ export const AssignedEssaySelect: FC<AssignedEssaySelectProps> = () => {
                   </CompletionMessage>
                 </AssignmentTypeContentContainer>
               )} */}
-            </>
-          )}
-        </>
-      )}
-    </>
-  )
+						</>
+					)}
+				</>
+			)}
+		</>
+	)
 }
