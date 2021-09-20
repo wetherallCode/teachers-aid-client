f46088109d3306fa601b49ea6d8111c1197e3204
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-19 20:41:43 -0400

    formatting

--- src/components/dashboard/teacher/teachers-aid/class-control-panel/center-console/PeriodSelectorDisplay.tsx
+++ src/components/dashboard/teacher/teachers-aid/class-control-panel/center-console/PeriodSelectorDisplay.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react'
 import { gql, useLazyQuery } from '@apollo/client'
 import {
-	findCourseInfoByCourseId,
-	findCourseInfoByCourseIdVariables,
-	me_me_Teacher,
+  findCourseInfoByCourseId,
+  findCourseInfoByCourseIdVariables,
+  me_me_Teacher,
 } from '../../../../../../schemaTypes'
 import { useTeachersAidContextProvider } from '../../state/TeachersAidContext'
 import { CourseSelectButton } from '../../styles/classControlPanelStyles'
@@ -12,100 +12,101 @@ import { useUserContextProvider } from '../../../../../../contexts/UserContext'
 export type PeriodSelectorDisplayProps = {}
 
 export const GET_COURSE_INFO_QUERY = gql`
-	query findCourseInfoByCourseId($input: FindCourseInfoByCourseIdInput!) {
-		findCourseInfoByCourseId(input: $input) {
-			courseInfo {
-				_id
-				startsAt
-				course {
-					_id
-					name
-					hasStudents {
-						_id
-						firstName
-						schoolId
-						lastName
-						cohort
-						hasAbsences {
-							dayAbsent
-						}
-						hasProtocols {
-							_id
-							student {
-								_id
-								firstName
-								lastName
-							}
-							isActive
-							response
-						}
-					}
-				}
-				endsAt
-				schoolDayType
-				cohortBasedSeating
-				assignedSeats {
-					deskNumber
-					redCohortStudent {
-						_id
-						firstName
-						lastName
-						cohort
-						hasAbsences {
-							dayAbsent
-						}
-					}
-					whiteCohortStudent {
-						_id
-						firstName
-						lastName
-						cohort
-						hasAbsences {
-							dayAbsent
-						}
-					}
-					student {
-						_id
-						firstName
-						lastName
-						cohort
-						hasAbsences {
-							dayAbsent
-						}
-					}
-				}
-			}
-		}
-	}
+  query findCourseInfoByCourseId($input: FindCourseInfoByCourseIdInput!) {
+    findCourseInfoByCourseId(input: $input) {
+      courseInfo {
+        _id
+        startsAt
+        course {
+          _id
+          name
+          hasStudents {
+            _id
+            firstName
+            schoolId
+            lastName
+            cohort
+            hasAbsences {
+              dayAbsent
+            }
+            hasProtocols {
+              _id
+              student {
+                _id
+                firstName
+                lastName
+              }
+              isActive
+              response
+            }
+          }
+        }
+        endsAt
+        schoolDayType
+        cohortBasedSeating
+        assignedSeats {
+          deskNumber
+          redCohortStudent {
+            _id
+            firstName
+            lastName
+            cohort
+            hasAbsences {
+              dayAbsent
+            }
+          }
+          whiteCohortStudent {
+            _id
+            firstName
+            lastName
+            cohort
+            hasAbsences {
+              dayAbsent
+            }
+          }
+          student {
+            _id
+            firstName
+            lastName
+            cohort
+            hasAbsences {
+              dayAbsent
+            }
+          }
+        }
+      }
+    }
+  }
 `
 export const PeriodSelectorDisplay: FC<PeriodSelectorDisplayProps> = () => {
-	const me: me_me_Teacher = useUserContextProvider()
-	const [, event] = useTeachersAidContextProvider()
+  const me: me_me_Teacher = useUserContextProvider()
+  const [, event] = useTeachersAidContextProvider()
 
-	const [loadCourse, { data }] = useLazyQuery<
-		findCourseInfoByCourseId,
-		findCourseInfoByCourseIdVariables
-	>(GET_COURSE_INFO_QUERY, {
-		onCompleted: (data) =>
-			event({
-				type: 'SET_COURSE',
-				payload: data.findCourseInfoByCourseId.courseInfo,
-			}),
-		pollInterval: 1000,
-		onError: (error) => console.error(error),
-	})
+  const [loadCourse, { data }] = useLazyQuery<
+    findCourseInfoByCourseId,
+    findCourseInfoByCourseIdVariables
+  >(GET_COURSE_INFO_QUERY, {
+    onCompleted: (data) =>
+      event({
+        type: 'SET_COURSE',
+        payload: data.findCourseInfoByCourseId.courseInfo,
+      }),
+    pollInterval: 1000,
+    onError: (error) => console.error(error),
+  })
 
-	return (
-		<>
-			{me.teachesCourses.slice(2).map((course) => (
-				<CourseSelectButton
-					key={course._id!}
-					onClick={() => {
-						loadCourse({ variables: { input: { courseId: course._id! } } })
-					}}>
-					{course.name}
-				</CourseSelectButton>
-			))}
-		</>
-	)
+  return (
+    <>
+      {me.teachesCourses.slice(2).map((course) => (
+        <CourseSelectButton
+          key={course._id!}
+          onClick={() => {
+            loadCourse({ variables: { input: { courseId: course._id! } } })
+          }}
+        >
+          {course.name}
+        </CourseSelectButton>
+      ))}
+    </>
+  )
 }
