cbbf397a2d55d9d5237c6f4cf60673fb766e3994
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-19 20:44:36 -0400

    beginning work on Behavior

--- src/components/dashboard/teacher/teachers-aid/student-info/behavior/DailyBehavior.tsx
+++ src/components/dashboard/teacher/teachers-aid/student-info/behavior/DailyBehavior.tsx
@@ -1,6 +1,17 @@
+import { gql } from '@apollo/client'
 import React from 'react'
 
 export type DailyBehaviorProps = {}
+
+export const CREATE_BEHAVIOR_MUTATION = gql`
+  mutation createStudentBehavior($input: CreateStudentBehaviorInput!) {
+    createStudentBehavior(input: $input) {
+      studentBehavior {
+        _id
+      }
+    }
+  }
+`
 
 export const DailyBehavior = ({}: DailyBehaviorProps) => {
   return (
