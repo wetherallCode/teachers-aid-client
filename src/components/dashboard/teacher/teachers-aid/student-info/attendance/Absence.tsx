a0694c1d1b51878088c788476a271cc0eac251d2
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-19 20:43:39 -0400

    added absenceCheck

--- src/components/dashboard/teacher/teachers-aid/student-info/attendance/Absence.tsx
+++ src/components/dashboard/teacher/teachers-aid/student-info/attendance/Absence.tsx
@@ -1,81 +1,86 @@
 import { MutationFunctionOptions, useMutation } from '@apollo/client'
 import React from 'react'
 import {
-	createAbsenceVariables,
-	createAbsence,
-	MarkingPeriodEnum,
-	findStudentInfoByStudentId_findStudentById_student,
-	removeAbsence,
-	removeAbsenceVariables,
+  createAbsenceVariables,
+  createAbsence,
+  MarkingPeriodEnum,
+  findStudentInfoByStudentId_findStudentById_student,
+  removeAbsence,
+  removeAbsenceVariables,
 } from '../../../../../../schemaTypes'
 import { todaysLocaleDate } from '../../../../../../utils'
 import { AttendanceButton } from '../../styles/studentInfoStyles'
-import { CREATE_STUDENT_ABSENCE_MUTATION, RemoveAbsenceType } from './DailyAttendance'
+import {
+  CREATE_STUDENT_ABSENCE_MUTATION,
+  RemoveAbsenceType,
+} from './DailyAttendance'
 import { RemoveAbsence } from './RemoveAbsence'
 
 export type AbsenceProps = {
-	currentMarkingPeriod: MarkingPeriodEnum
-	student: findStudentInfoByStudentId_findStudentById_student
-	removeAbsence: RemoveAbsenceType
-	todaysAbsenceId: string
-	unexcusedLatenessId: string
-	excusedLatenessId: string
-	removeAbsenceCalled: boolean
+  currentMarkingPeriod: MarkingPeriodEnum
+  student: findStudentInfoByStudentId_findStudentById_student
+  removeAbsence: RemoveAbsenceType
+  todaysAbsenceId: string
+  unexcusedLatenessId: string
+  excusedLatenessId: string
+  removeAbsenceCalled: boolean
+  absenceCheck: boolean
 }
 
 export const Absence = ({
-	currentMarkingPeriod,
-	student,
-	removeAbsence,
-	todaysAbsenceId,
-	unexcusedLatenessId,
-	excusedLatenessId,
-	removeAbsenceCalled,
+  currentMarkingPeriod,
+  student,
+  removeAbsence,
+  todaysAbsenceId,
+  unexcusedLatenessId,
+  excusedLatenessId,
+  removeAbsenceCalled,
+  absenceCheck,
 }: AbsenceProps) => {
-	const [createAbsence, { data, called }] = useMutation<createAbsence, createAbsenceVariables>(
-		CREATE_STUDENT_ABSENCE_MUTATION,
-		{
-			variables: {
-				input: {
-					dayAbsent: todaysLocaleDate,
-					markingPeriod: currentMarkingPeriod,
-					studentId: student._id!,
-				},
-			},
-			onCompleted: (data) => console.log(data),
-			refetchQueries: [
-				'findStudentInfoByStudentIdForDesk',
-				'findStudentInfoByStudentId',
-				'findCourseByIdForTeachersAid',
-			],
-		}
-	)
-	console.log(
-		new Date().toLocaleDateString(),
-		student.hasAbsences.some((absence) => absence.dayAbsent === new Date().toLocaleDateString())
-	)
-	return (
-		<>
-			{student.hasAbsences.length === 0 ||
-			student.hasAbsences.some(
-				(absence) => absence.dayAbsent !== new Date().toLocaleDateString()
-			) ? (
-				<AttendanceButton
-					lateButton={false}
-					created={false}
-					onClick={() => {
-						!unexcusedLatenessId && !excusedLatenessId && createAbsence()
-					}}>
-					{/* {called ? '...' : 'Create Absence'} */}
-					Create Absence
-				</AttendanceButton>
-			) : (
-				<RemoveAbsence
-					absenceId={todaysAbsenceId}
-					removeAbsence={removeAbsence}
-					called={removeAbsenceCalled}
-				/>
-			)}
-		</>
-	)
+  const [createAbsence, { data, called }] = useMutation<
+    createAbsence,
+    createAbsenceVariables
+  >(CREATE_STUDENT_ABSENCE_MUTATION, {
+    variables: {
+      input: {
+        dayAbsent: todaysLocaleDate,
+        markingPeriod: currentMarkingPeriod,
+        studentId: student._id!,
+      },
+    },
+    onCompleted: (data) => console.log(data),
+    refetchQueries: [
+      'findStudentInfoByStudentIdForDesk',
+      'findStudentInfoByStudentId',
+      'findCourseByIdForTeachersAid',
+    ],
+  })
+  // console.log(
+  //   new Date().toLocaleDateString(),
+  //   student.hasAbsences.some(
+  //     (absence) => absence.dayAbsent === new Date().toLocaleDateString()
+  //   )
+  // )
+  // console.log(absenceCheck)
+  return (
+    <>
+      {!absenceCheck ? (
+        <AttendanceButton
+          lateButton={false}
+          created={false}
+          onClick={() => {
+            !unexcusedLatenessId && !excusedLatenessId && createAbsence()
+          }}
+        >
+          Create Absence
+        </AttendanceButton>
+      ) : (
+        <RemoveAbsence
+          absenceId={todaysAbsenceId}
+          removeAbsence={removeAbsence}
+          called={removeAbsenceCalled}
+        />
+      )}
+    </>
+  )
 }
