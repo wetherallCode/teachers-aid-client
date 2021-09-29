e53e6781dabb8f868cbd46037545cb020cd17e82
Rob Wetherall <robertgwetherall1@gmail.com>
2021-09-29 18:41:39 -0400

    started work on student grade display

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import {
-  HomeScreenTitle,
-  GetStartedButton,
-  StyledLink,
-  HomeScreenContainer,
-  PasswordContainer,
-  GetStartedButtonContainer,
-  GetAssignmentsButton,
+	HomeScreenTitle,
+	GetStartedButton,
+	StyledLink,
+	HomeScreenContainer,
+	PasswordContainer,
+	GetStartedButtonContainer,
+	GetAssignmentsButton,
 } from './homeStyles'
 import { useUserContextProvider } from '../../contexts/UserContext'
 import { Modal } from '../../animations/Modal'
@@ -15,61 +15,67 @@ import { useToggle } from '../../hooks'
 import { me_me } from '../../schemaTypes'
 import { InitialPasswordChange } from './InitialPasswordChange'
 import { PasswordCheck } from './PasswordCheck'
+import { useGradeCalculator } from '../../hooks/useGradeCalculator'
+import { useMarkingPeriodContextProvider } from '../../contexts/markingPeriod/MarkingPeriodContext'
 
 export const Home = () => {
-  const me: me_me = useUserContextProvider()
-  const [isLoginVisible, toggleLogin] = useToggle(false)
-  const [passwordCheck, setPasswordCheck] = useState(false)
+	const me: me_me = useUserContextProvider()
+	const [currentMarkingPeriod] = useMarkingPeriodContextProvider()
+	const [isLoginVisible, toggleLogin] = useToggle(false)
+	const [passwordCheck, setPasswordCheck] = useState(false)
 
-  const student = me && me.__typename === 'Student'
-
-  return (
-    <>
-      <HomeScreenContainer>
-        <>
-          <HomeScreenTitle>Welcome to Mr. Wetherall's Class</HomeScreenTitle>
-          <>
-            {me ? (
-              <>
-                {me !== null && (
-                  <PasswordCheck me={me} setPasswordCheck={setPasswordCheck} />
-                )}
-                <PasswordContainer>
-                  {!passwordCheck ? (
-                    <GetStartedButtonContainer>
-                      <StyledLink to='/lesson-home'>
-                        <GetStartedButton>Go to Lesson</GetStartedButton>
-                      </StyledLink>
-                      {student && (
-                        <StyledLink to='/dashboard/assignments'>
-                          <GetAssignmentsButton>
-                            Get Assignments
-                          </GetAssignmentsButton>
-                        </StyledLink>
-                      )}
-                    </GetStartedButtonContainer>
-                  ) : (
-                    <InitialPasswordChange me={me} />
-                  )}
-                </PasswordContainer>
-              </>
-            ) : (
-              <>
-                {!passwordCheck && (
-                  <PasswordContainer>
-                    <GetStartedButton onClick={toggleLogin}>
-                      Get Started
-                    </GetStartedButton>
-                  </PasswordContainer>
-                )}
-              </>
-            )}
-          </>
-        </>
-      </HomeScreenContainer>
-      <Modal isToggled={isLoginVisible} setIsToggled={toggleLogin}>
-        <Login toggleLogin={toggleLogin} />
-      </Modal>
-    </>
-  )
+	const student = me && me.__typename === 'Student'
+	// const grade = useGradeCalculator({
+	// 	studentId: me.__typename === 'Student' && me._id!,
+	// 	polling: true,
+	// 	pollInterval: 5000,
+	// 	markingPeriod: currentMarkingPeriod,
+	// })
+	// console.log(grade)
+	return (
+		<>
+			<HomeScreenContainer>
+				<>
+					<HomeScreenTitle>Welcome to Mr. Wetherall's Class</HomeScreenTitle>
+					<>
+						{me ? (
+							<>
+								{me !== null && <PasswordCheck me={me} setPasswordCheck={setPasswordCheck} />}
+								<PasswordContainer>
+									{!passwordCheck ? (
+										<GetStartedButtonContainer>
+											<StyledLink to='/lesson-home'>
+												<GetStartedButton>Go to Lesson</GetStartedButton>
+											</StyledLink>
+											{student && (
+												<>
+													<StyledLink to='/dashboard/assignments'>
+														<GetAssignmentsButton>Get Assignments</GetAssignmentsButton>
+													</StyledLink>
+													<div>{}</div>
+												</>
+											)}
+										</GetStartedButtonContainer>
+									) : (
+										<InitialPasswordChange me={me} />
+									)}
+								</PasswordContainer>
+							</>
+						) : (
+							<>
+								{!passwordCheck && (
+									<PasswordContainer>
+										<GetStartedButton onClick={toggleLogin}>Get Started</GetStartedButton>
+									</PasswordContainer>
+								)}
+							</>
+						)}
+					</>
+				</>
+			</HomeScreenContainer>
+			<Modal isToggled={isLoginVisible} setIsToggled={toggleLogin}>
+				<Login toggleLogin={toggleLogin} />
+			</Modal>
+		</>
+	)
 }
