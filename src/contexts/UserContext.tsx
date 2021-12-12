1e26dcaff7fbabb399c190105e5403ec9eeb9032
Rob Wetherall <robertgwetherall1@gmail.com>
2021-12-12 15:41:46 -0500

    added assignmentsInClassNotAllowed under courseInfo

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -38,6 +38,7 @@ export const ME_QUERY = gql`
             halfDayStartsAt
             halfDayEndsAt
             schoolDayType
+            assignmentsInClassNotAllowed
           }
         }
         hasWritingMetrics {
