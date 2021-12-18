be8df478e4d22d1ea6db6539bc34936a43b4f6e4
Rob Wetherall <robertgwetherall1@gmail.com>
2021-12-18 18:35:25 -0500

    draft should change with draft selector

--- src/components/dashboard/student/assignments/essays/completed-essays/CompletedEssayViewer.tsx
+++ src/components/dashboard/student/assignments/essays/completed-essays/CompletedEssayViewer.tsx
@@ -6,10 +6,10 @@ import { createEditor, Node } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 
 import {
-	findEssayById_findEssayById_essay,
-	UpdateWorkingDraft,
-	UpdateWorkingDraftVariables,
-	SubmittedFinalDraftsInput,
+  findEssayById_findEssayById_essay,
+  UpdateWorkingDraft,
+  UpdateWorkingDraftVariables,
+  SubmittedFinalDraftsInput,
 } from '../../../../../../schemaTypes'
 
 import { useMutation } from '@apollo/client'
@@ -22,116 +22,119 @@ import { Leaf } from '../../../../teacher/assignments/grade-assignments/essays/e
 // }
 
 export type CompletedEssayViewerProps = {
-	draft: string
+  draft: string
 }
 
 export type submittedFinalDraftType = {
-	draft: string
-	gradingDraft: string
-	comments: string[]
-	score: number
+  draft: string
+  gradingDraft: string
+  comments: string[]
+  score: number
 }
 
-export const CompletedEssayViewer: FC<CompletedEssayViewerProps> = ({ draft }) => {
-	const editor = useMemo(() => withReact(createEditor()), [])
+export const CompletedEssayViewer: FC<CompletedEssayViewerProps> = ({
+  draft,
+}) => {
+  const editor = useMemo(() => withReact(createEditor()), [])
 
-	const parsedEssay = JSON.parse(draft)
-	const [value, setValue] = useState(parsedEssay)
-	const content = JSON.stringify(value)
+  const parsedEssay = JSON.parse(draft)
 
-	const renderElement = useCallback((props) => {
-		switch (props.element.type) {
-			case 'code':
-				return <CodeElement {...props} />
-			default:
-				return <DefaultElement {...props} />
-		}
-	}, [])
+  const [value, setValue] = useState(parsedEssay)
 
-	const renderLeaf = useCallback((props) => {
-		return <Leaf {...props} />
-	}, [])
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
 
-	return (
-		<EssaySheet style={{ userSelect: 'all' }}>
-			<Slate
-				editor={editor}
-				value={value as Node[]}
-				onChange={(value: any) => {
-					setValue(value as Node[])
-				}}>
-				<Editable
-					renderElement={renderElement}
-					spellCheck
-					onSelect={(e: any) => {}}
-					onCopy={(e: any) => {
-						e.preventDefault()
-					}}
-					autoFocus={true}
-					renderLeaf={renderLeaf}
-					style={{
-						height: '98%',
-						userSelect: 'none',
-					}}
-					placeholder={`Your essay goes here; let's get started...`}
-					onKeyDown={(e) => {
-						// e.preventDefault()
-						// if (e.key === 'Enter') return null
-						// updateWorkingDraft()
-						// if (!e.ctrlKey) {
-						// return
-						// }
-						// switch (e.key) {
-						//   case '`': {
-						//     e.preventDefault()
-						//     const [match] = Editor.nodes(editor, {
-						//       match: (n) => n.type === 'code',
-						//     })
-						//     Transforms.setNodes(
-						//       editor,
-						//       { type: match ? 'paragraph' : 'code' },
-						//       { match: (n) => Editor.isBlock(editor, n) }
-						//     )
-						//     break
-						//   }
-						//   case 'b': {
-						//     e.preventDefault()
-						//     CustomEditor.toggleBoldMark(editor)
-						//     break
-						//   }
-						//   case 's': {
-						//     e.preventDefault()
-						//     CustomEditor.toggleStrikeThrough(editor)
-						//     break
-						//   }
-						//   case 'u': {
-						//     e.preventDefault()
-						//     CustomEditor.toggleUnderline(editor)
-						//     break
-						//   }
-						// case '17': {
-						//   e.preventDefault()
-						// }
-						// }
-					}}
-				/>
-			</Slate>
-		</EssaySheet>
-	)
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />
+  }, [])
+
+  return (
+    <EssaySheet style={{ userSelect: 'all' }}>
+      <Slate
+        editor={editor}
+        value={parsedEssay as Node[]}
+        onChange={(value: any) => {
+          setValue(value as Node[])
+        }}
+      >
+        <Editable
+          renderElement={renderElement}
+          spellCheck
+          onSelect={(e: any) => e.preventDefault()}
+          onCopy={(e: any) => {
+            e.preventDefault()
+          }}
+          autoFocus={true}
+          renderLeaf={renderLeaf}
+          style={{
+            height: '98%',
+            userSelect: 'none',
+          }}
+          placeholder={`Your essay goes here; let's get started...`}
+          onKeyDown={(e) => {
+            // e.preventDefault()
+            // if (e.key === 'Enter') return null
+            // updateWorkingDraft()
+            // if (!e.ctrlKey) {
+            // return
+            // }
+            // switch (e.key) {
+            //   case '`': {
+            //     e.preventDefault()
+            //     const [match] = Editor.nodes(editor, {
+            //       match: (n) => n.type === 'code',
+            //     })
+            //     Transforms.setNodes(
+            //       editor,
+            //       { type: match ? 'paragraph' : 'code' },
+            //       { match: (n) => Editor.isBlock(editor, n) }
+            //     )
+            //     break
+            //   }
+            //   case 'b': {
+            //     e.preventDefault()
+            //     CustomEditor.toggleBoldMark(editor)
+            //     break
+            //   }
+            //   case 's': {
+            //     e.preventDefault()
+            //     CustomEditor.toggleStrikeThrough(editor)
+            //     break
+            //   }
+            //   case 'u': {
+            //     e.preventDefault()
+            //     CustomEditor.toggleUnderline(editor)
+            //     break
+            //   }
+            // case '17': {
+            //   e.preventDefault()
+            // }
+            // }
+          }}
+        />
+      </Slate>
+    </EssaySheet>
+  )
 }
 const CodeElement = (props: any) => {
-	return (
-		<pre {...props.attributes}>
-			<code>{props.children}</code>
-		</pre>
-	)
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
 }
 const DefaultElement = (props: any) => {
-	return (
-		<p style={{ textIndent: '2%', userSelect: 'none' }} {...props.attributes}>
-			{props.children}
-		</p>
-	)
+  return (
+    <p style={{ textIndent: '2%', userSelect: 'none' }} {...props.attributes}>
+      {props.children}
+    </p>
+  )
 }
 
 // export const Leaf = (props: any) => {
