9aa0a28b1bcb7c69f38fedda8ce3855fe4967a28
Rob Wetherall <robertgwetherall1@gmail.com>
2022-06-14 17:45:45 -0400

    start class functionality wip

--- src/components/dashboard/teacher/teachers-aid/class-control-panel/DynamicLesson/DynamicLessonManager.tsx
+++ src/components/dashboard/teacher/teachers-aid/class-control-panel/DynamicLesson/DynamicLessonManager.tsx
@@ -1,130 +1,127 @@
 import React, { FC } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import {
-  updateDynamicLesson,
-  updateDynamicLessonVariables,
-  findLessonByCourseAndDate_findLessonByCourseAndDate_lesson,
-  DynamicLessonEnums,
+	updateDynamicLesson,
+	updateDynamicLessonVariables,
+	findLessonByCourseAndDate_findLessonByCourseAndDate_lesson,
+	DynamicLessonEnums,
+	createStudentBehavior,
+	createStudentBehaviorVariables,
 } from '../../../../../../schemaTypes'
 import {
-  DynamicLessonHeader,
-  DynamicLessonButton,
-  DynamicLessonContainer,
-  DynamicLessonOnButton,
-  DynamicLessonOffButtonContainer,
-  DynamicLessonOffButton,
-  DynamicLessonButtonContainer,
+	DynamicLessonHeader,
+	DynamicLessonButton,
+	DynamicLessonContainer,
+	DynamicLessonOnButton,
+	DynamicLessonOffButtonContainer,
+	DynamicLessonOffButton,
+	DynamicLessonButtonContainer,
 } from '../../styles/classControlPanelStyles'
 import { WarmUp } from '../../../../../lesson/lesson-components/WarmUp'
 import { WarmUpManager } from '../warmup-manager/WarmUpManager'
 import { CoolDownManager } from '../coolDown-manager/CoolDownManager'
 import { StudentInfoSelector } from '../center-console/StudentInfoSelector'
+import { CREATE_BEHAVIOR_MUTATION } from '../../student-info/behavior/DailyBehavior'
 
 export type DynamicLessonManagerProps = {
-  lesson: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson
+	lesson: findLessonByCourseAndDate_findLessonByCourseAndDate_lesson
+	presentStudentList: string[]
 }
 
 export type DynamicLessonButtonProps = {
-  currentLessonSetting?: boolean
+	currentLessonSetting?: boolean
 }
 
 export const UPDATE_DYNAMIC_LESSON_MUTATION = gql`
-  mutation updateDynamicLesson($input: UpdateDynamicLessonInput!) {
-    UpdateDynamicLesson(input: $input) {
-      lesson {
-        _id
-        dynamicLesson
-      }
-    }
-  }
+	mutation updateDynamicLesson($input: UpdateDynamicLessonInput!) {
+		UpdateDynamicLesson(input: $input) {
+			lesson {
+				_id
+				dynamicLesson
+			}
+		}
+	}
 `
 
-export const DynamicLessonManager = ({ lesson }: DynamicLessonManagerProps) => {
-  const [updateDynamicLeson] = useMutation<
-    updateDynamicLesson,
-    updateDynamicLessonVariables
-  >(UPDATE_DYNAMIC_LESSON_MUTATION, {
-    onCompleted: (data) => console.log(data.UpdateDynamicLesson),
-    refetchQueries: ['findLessonByCourseAndDate'],
-  })
+export const DynamicLessonManager = ({ lesson, presentStudentList }: DynamicLessonManagerProps) => {
+	const [updateDynamicLeson] = useMutation<updateDynamicLesson, updateDynamicLessonVariables>(
+		UPDATE_DYNAMIC_LESSON_MUTATION,
+		{
+			onCompleted: (data) => console.log(data.UpdateDynamicLesson),
+			refetchQueries: ['findLessonByCourseAndDate'],
+		}
+	)
 
-  const handleClick = (dynamicLessonSetting: DynamicLessonEnums) => {
-    updateDynamicLeson({
-      variables: {
-        input: {
-          lessonId: lesson._id!,
-          dynamicLessonUpdate: dynamicLessonSetting,
-        },
-      },
-    })
-  }
+	const handleClick = (dynamicLessonSetting: DynamicLessonEnums) => {
+		updateDynamicLeson({
+			variables: {
+				input: {
+					lessonId: lesson._id!,
+					dynamicLessonUpdate: dynamicLessonSetting,
+				},
+			},
+		})
+		// createBatchStudentBehavior()
+	}
 
-  return (
-    <DynamicLessonContainer>
-      <DynamicLessonHeader>Dynamic Lesson</DynamicLessonHeader>
-      <div
-        style={{ gridColumn: '1/-1', display: 'grid', justifyItems: 'center' }}
-      >
-        <StudentInfoSelector />
-      </div>
-      {lesson.dynamicLesson === 'OFF' ? (
-        <DynamicLessonOnButton
-          // currentLessonSetting={}
-          onClick={() => handleClick(DynamicLessonEnums.ON)}
-        >
-          Start Lesson
-        </DynamicLessonOnButton>
-      ) : (
-        <>
-          <DynamicLessonButtonContainer>
-            {lesson.dynamicLesson === 'WARM_UP' ? (
-              <WarmUpManager lesson={lesson} />
-            ) : (
-              <DynamicLessonButton
-                // currentLessonSetting={lesson.dynamicLesson === 'WARM_UP'}
-                onClick={() => handleClick(DynamicLessonEnums.WARM_UP)}
-              >
-                Warm Up
-              </DynamicLessonButton>
-            )}
-          </DynamicLessonButtonContainer>
-          <DynamicLessonButton
-            currentLessonSetting={lesson.dynamicLesson === 'LESSON_DETAILS'}
-            onClick={() => handleClick(DynamicLessonEnums.LESSON_DETAILS)}
-          >
-            Lesson Details
-          </DynamicLessonButton>
-          <DynamicLessonButton
-            currentLessonSetting={lesson.dynamicLesson === 'VOCAB'}
-            onClick={() => handleClick(DynamicLessonEnums.VOCAB)}
-          >
-            Vocab
-          </DynamicLessonButton>
-          <DynamicLessonButtonContainer>
-            {lesson.dynamicLesson === 'EXIT_ACTIVITY' ? (
-              <CoolDownManager lesson={lesson} />
-            ) : (
-              <DynamicLessonButton
-                // currentLessonSetting={lesson.dynamicLesson === 'EXIT_ACTIVITY'}
-                onClick={() => handleClick(DynamicLessonEnums.EXIT_ACTIVITY)}
-              >
-                Cool Down
-              </DynamicLessonButton>
-            )}
-          </DynamicLessonButtonContainer>
-          <DynamicLessonOffButtonContainer>
-            <DynamicLessonOffButton
-              // currentLessonSetting={}
-              onClick={() => {
-                handleClick(DynamicLessonEnums.OFF)
-                console.log('click')
-              }}
-            >
-              End Lesson
-            </DynamicLessonOffButton>
-          </DynamicLessonOffButtonContainer>
-        </>
-      )}
-    </DynamicLessonContainer>
-  )
+	return (
+		<DynamicLessonContainer>
+			<DynamicLessonHeader>Dynamic Lesson</DynamicLessonHeader>
+			<div style={{ gridColumn: '1/-1', display: 'grid', justifyItems: 'center' }}>
+				<StudentInfoSelector />
+			</div>
+			{lesson.dynamicLesson === 'OFF' ? (
+				<DynamicLessonOnButton
+					// currentLessonSetting={}
+					onClick={() => handleClick(DynamicLessonEnums.ON)}>
+					Start Lesson
+				</DynamicLessonOnButton>
+			) : (
+				<>
+					<DynamicLessonButtonContainer>
+						{lesson.dynamicLesson === 'WARM_UP' ? (
+							<WarmUpManager lesson={lesson} />
+						) : (
+							<DynamicLessonButton
+								// currentLessonSetting={lesson.dynamicLesson === 'WARM_UP'}
+								onClick={() => handleClick(DynamicLessonEnums.WARM_UP)}>
+								Warm Up
+							</DynamicLessonButton>
+						)}
+					</DynamicLessonButtonContainer>
+					<DynamicLessonButton
+						currentLessonSetting={lesson.dynamicLesson === 'LESSON_DETAILS'}
+						onClick={() => handleClick(DynamicLessonEnums.LESSON_DETAILS)}>
+						Lesson Details
+					</DynamicLessonButton>
+					<DynamicLessonButton
+						currentLessonSetting={lesson.dynamicLesson === 'VOCAB'}
+						onClick={() => handleClick(DynamicLessonEnums.VOCAB)}>
+						Vocab
+					</DynamicLessonButton>
+					<DynamicLessonButtonContainer>
+						{lesson.dynamicLesson === 'EXIT_ACTIVITY' ? (
+							<CoolDownManager lesson={lesson} />
+						) : (
+							<DynamicLessonButton
+								// currentLessonSetting={lesson.dynamicLesson === 'EXIT_ACTIVITY'}
+								onClick={() => handleClick(DynamicLessonEnums.EXIT_ACTIVITY)}>
+								Cool Down
+							</DynamicLessonButton>
+						)}
+					</DynamicLessonButtonContainer>
+					<DynamicLessonOffButtonContainer>
+						<DynamicLessonOffButton
+							// currentLessonSetting={}
+							onClick={() => {
+								handleClick(DynamicLessonEnums.OFF)
+								console.log('click')
+							}}>
+							End Lesson
+						</DynamicLessonOffButton>
+					</DynamicLessonOffButtonContainer>
+				</>
+			)}
+		</DynamicLessonContainer>
+	)
 }
--- src/components/dashboard/teacher/teachers-aid/class-control-panel/center-console/ControlPanelDisplay.tsx
+++ src/components/dashboard/teacher/teachers-aid/class-control-panel/center-console/ControlPanelDisplay.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react'
 import {
-  PeriodSelectDisplayContainer,
-  ProtocolManagerContainer,
-  DynamicLessonContainer,
-  CenteredDiv,
-  MainScreenManagerContainer,
+	PeriodSelectDisplayContainer,
+	ProtocolManagerContainer,
+	DynamicLessonContainer,
+	CenteredDiv,
+	MainScreenManagerContainer,
 } from '../../styles/classControlPanelStyles'
 import { useTeachersAidContextProvider } from '../../state/TeachersAidContext'
 import { PeriodSelectorDisplay } from './PeriodSelectorDisplay'
 import { ProtocolManager } from '../protocol-manager/ProtocolManager'
 import { DynamicLessonManager } from '../DynamicLesson/DynamicLessonManager'
 import {
-  findLessonByCourseAndDate,
-  findLessonByCourseAndDateVariables,
+	findLessonByCourseAndDate,
+	findLessonByCourseAndDateVariables,
 } from '../../../../../../schemaTypes'
 
 import { date } from '../../../../../../utils'
@@ -24,107 +24,100 @@ import { StudentInfoSelector } from './StudentInfoSelector'
 
 export type ControlPanelDisplayProps = { presentStudentList: string[] }
 
-export const ControlPanelDisplay = ({
-  presentStudentList,
-}: ControlPanelDisplayProps) => {
-  const [state, event] = useTeachersAidContextProvider()
+export const ControlPanelDisplay = ({ presentStudentList }: ControlPanelDisplayProps) => {
+	const [state, event] = useTeachersAidContextProvider()
 
-  const { loading, data } = useQuery<
-    findLessonByCourseAndDate,
-    findLessonByCourseAndDateVariables
-  >(FIND_LESSON_QUERY, {
-    variables: {
-      input: {
-        courseId: state.context.courseInfo!.course._id!,
-        lessonDate: date,
-      },
-    },
-    onCompleted: (data) => {
-      data.findLessonByCourseAndDate.lesson &&
-        event({
-          type: 'LOAD_PROTOCOLS',
-          payload: data.findLessonByCourseAndDate.lesson.duringActivities,
-        })
-      data.findLessonByCourseAndDate.lesson &&
-        event({
-          type: 'SET_LESSON_ID',
-          payload: data.findLessonByCourseAndDate.lesson._id!,
-        })
-    },
-    pollInterval: 1000,
-    onError: (error) => console.error(error),
-  })
+	const { loading, data } = useQuery<findLessonByCourseAndDate, findLessonByCourseAndDateVariables>(
+		FIND_LESSON_QUERY,
+		{
+			variables: {
+				input: {
+					courseId: state.context.courseInfo!.course._id!,
+					lessonDate: date,
+				},
+			},
+			onCompleted: (data) => {
+				data.findLessonByCourseAndDate.lesson &&
+					event({
+						type: 'LOAD_PROTOCOLS',
+						payload: data.findLessonByCourseAndDate.lesson.duringActivities,
+					})
+				data.findLessonByCourseAndDate.lesson &&
+					event({
+						type: 'SET_LESSON_ID',
+						payload: data.findLessonByCourseAndDate.lesson._id!,
+					})
+			},
+			pollInterval: 1000,
+			onError: (error) => console.error(error),
+		}
+	)
 
-  if (loading)
-    return (
-      <CenteredDiv style={{ display: 'grid', gridTemplateRows: '1fr 4fr' }}>
-        <StudentInfoSelector />
-        <div>No Lesson Scheduled for Today</div>
-      </CenteredDiv>
-    )
+	if (loading)
+		return (
+			<CenteredDiv style={{ display: 'grid', gridTemplateRows: '1fr 4fr' }}>
+				<StudentInfoSelector />
+				<div>No Lesson Scheduled for Today</div>
+			</CenteredDiv>
+		)
 
-  return (
-    <>
-      {state.matches('controlPanelActions.dynamicLesson') &&
-        state.context.courseInfo._id &&
-        !state.context.courseSelectVisible && (
-          <>
-            {data?.findLessonByCourseAndDate.lesson ? (
-              <DynamicLessonManager
-                lesson={data?.findLessonByCourseAndDate.lesson!}
-              />
-            ) : (
-              <CenteredDiv
-                style={{ display: 'grid', gridTemplateRows: '1fr 4fr' }}
-              >
-                <StudentInfoSelector />
-                <div>No Lesson Scheduled for Today</div>
-              </CenteredDiv>
-            )}
-          </>
-        )}
-      {state.matches('controlPanelActions.protocolManager')! &&
-        state.context.courseInfo._id! &&
-        !state.context.courseSelectVisible && (
-          <>
-            {data?.findLessonByCourseAndDate.lesson ? (
-              <>
-                <ProtocolManager
-                  protocols={
-                    data?.findLessonByCourseAndDate.lesson.duringActivities!
-                  }
-                  lesson={data?.findLessonByCourseAndDate.lesson!}
-                  presentStudentList={presentStudentList}
-                />
-                {/* <Protocols
+	return (
+		<>
+			{state.matches('controlPanelActions.dynamicLesson') &&
+				state.context.courseInfo._id &&
+				!state.context.courseSelectVisible && (
+					<>
+						{data?.findLessonByCourseAndDate.lesson ? (
+							<DynamicLessonManager
+								presentStudentList={presentStudentList!}
+								lesson={data?.findLessonByCourseAndDate.lesson!}
+							/>
+						) : (
+							<CenteredDiv style={{ display: 'grid', gridTemplateRows: '1fr 4fr' }}>
+								<StudentInfoSelector />
+								<div>No Lesson Scheduled for Today</div>
+							</CenteredDiv>
+						)}
+					</>
+				)}
+			{state.matches('controlPanelActions.protocolManager')! &&
+				state.context.courseInfo._id! &&
+				!state.context.courseSelectVisible && (
+					<>
+						{data?.findLessonByCourseAndDate.lesson ? (
+							<>
+								<ProtocolManager
+									protocols={data?.findLessonByCourseAndDate.lesson.duringActivities!}
+									lesson={data?.findLessonByCourseAndDate.lesson!}
+									presentStudentList={presentStudentList}
+								/>
+								{/* <Protocols
                   protocols={
                     data?.findLessonByCourseAndDate.lesson.duringActivities!
                   }
                   lesson={data?.findLessonByCourseAndDate.lesson!}
                 /> */}
-              </>
-            ) : (
-              <CenteredDiv>
-                <div>No Lesson Scheduled for Today</div>
-              </CenteredDiv>
-            )}
-          </>
-        )}
-      {state.matches('controlPanelActions.mainScreenManager')! &&
-        state.context.courseInfo._id &&
-        !state.context.courseSelectVisible && (
-          <MainScreenManagerContainer>
-            <MainScreenManager
-              lesson={data?.findLessonByCourseAndDate.lesson!}
-            />
-          </MainScreenManagerContainer>
-        )}
+							</>
+						) : (
+							<CenteredDiv>
+								<div>No Lesson Scheduled for Today</div>
+							</CenteredDiv>
+						)}
+					</>
+				)}
+			{state.matches('controlPanelActions.mainScreenManager')! &&
+				state.context.courseInfo._id &&
+				!state.context.courseSelectVisible && (
+					<MainScreenManagerContainer>
+						<MainScreenManager lesson={data?.findLessonByCourseAndDate.lesson!} />
+					</MainScreenManagerContainer>
+				)}
 
-      {state.context.courseSelectVisible && (
-        <PeriodSelectDisplayContainer>
-          <PeriodSelectorDisplay />
-        </PeriodSelectDisplayContainer>
-      )}
-    </>
-  )
+			{state.context.courseSelectVisible && (
+				<PeriodSelectDisplayContainer>
+					<PeriodSelectorDisplay />
+				</PeriodSelectDisplayContainer>
+			)}
+		</>
+	)
 }
