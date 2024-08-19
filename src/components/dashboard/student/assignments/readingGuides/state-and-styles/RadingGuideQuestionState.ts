52eafe1ff797453081f1f4a85ba91dc4d6dda2fb
Rob Wetherall <robertgwetherall1@gmail.com>
2024-08-19 14:53:08 -0400

    Changed questions for reading guides

--- src/components/dashboard/student/assignments/readingGuides/state-and-styles/RadingGuideQuestionState.ts
+++ src/components/dashboard/student/assignments/readingGuides/state-and-styles/RadingGuideQuestionState.ts
@@ -1,11 +1,7 @@
 export type ReadingGuideQuestionState =
   | 'SUMMARY'
   | 'WHAT_WAS_LEARNED'
-  | 'MAIN_IDEA'
   | 'SUBJECT_OF_SECTION'
-  | 'WHAT_DID_SUBJECT_DO'
-  | 'HOW_DID_SUBJECT_DO_IT'
-  | 'WHY_DID_SUBJECT_DO_IT'
   | 'PROBLEM'
   | 'WHY_PROBLEM'
   | 'SOLUTION'
@@ -32,67 +28,37 @@ export const readingGuideQuestions: ReadingGuideQuestionObjectType[] = [
       'This question is based on the your answer to the last question.',
   },
   {
-    questionType: 'MAIN_IDEA',
-    question:
-      'In one or two sentences, what was the main idea of the readings?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
-  },
-  {
     questionType: 'SUBJECT_OF_SECTION',
     question:
-      'Who are the readings about? Your answer can be individuals or groups of people or things if the text does not discuss people. Write as many as you think are important.',
+      'Who (people or groups of people) were the most important people in these sections?',
     directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
-  },
-  {
-    questionType: 'WHAT_DID_SUBJECT_DO',
-    question: 'What did the person/people you wrote in the last answer do?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
-  },
-  {
-    questionType: 'WHY_DID_SUBJECT_DO_IT',
-    question: 'Why did they do it?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
-  },
-  {
-    questionType: 'HOW_DID_SUBJECT_DO_IT',
-    question: 'How did they do it?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
+      'There may be multiple people, so pick who you think is most important. As you get used to this, try to figure out who caused or solved the biggest problem.',
   },
   {
     questionType: 'PROBLEM',
-    question: 'What problem is being faced by the people in the readings?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
+    question: 'What did these people do to solve their problems?',
+    directions: `Every section in the text discusses a problem or conflict, so see everyone's actions as solutions`,
+  },
+  {
+    questionType: 'SOLUTION',
+    question: `How was the problem solved, or how did they attempt to solve it?`,
+    directions: `If the reading doesn't show how they solved it, figure out a way the problem could have been solved.`,
   },
   {
     questionType: 'WHY_PROBLEM',
     question: 'What caused this problem to happen?',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
-  },
-  {
-    questionType: 'SOLUTION',
-    question:
-      "How was the problem solved, or did they attempt to solve it? If the reading doesn't show  how they solved it, figure out a way the problem could have been solved.",
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
+    directions: `This may go back before the assigned sections, so don't be scared to flip back a page or two`,
   },
   {
     questionType: 'SOLUTIONS_REASON',
     question: 'Why did/would the solution solve the problem',
-    directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
+    directions: `Sometimes our ideas don't work out, so many times you'll need to show why they thought their solution would work.`,
   },
   {
     questionType: 'ASSUMPTIONS',
     question:
       'What assumptions are the main characters in the reading making in their solution? If you came up with the solution, what assumptions are you using in your solution?',
     directions:
-      'Only use the assigned readings. Use of google will result in partial or no credit.',
+      'Everyone has to make guesses when they solve their problems. When they guess right - problem solved. When they guess wrong - problem not solved.',
   },
 ]
