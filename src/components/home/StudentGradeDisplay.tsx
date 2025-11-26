6d019547b26294e66af7545948ac0cc771e50fc9
Rob Wetherall <robertgwetherall1@gmail.com>
2025-11-26 11:52:40 -0500

    added extra credit to grade display

--- src/components/home/StudentGradeDisplay.tsx
+++ src/components/home/StudentGradeDisplay.tsx
@@ -3,6 +3,12 @@ import { useMarkingPeriodContextProvider } from '../../contexts/markingPeriod/Ma
 import { useCalculateGrades } from '../../hooks/useCalculateGrades'
 import { useGradeCalculator } from '../../hooks/useGradeCalculator'
 import { GradeDisplay, StudentGradeContainer } from './homeStyles'
+import { useQuery } from '@apollo/client'
+import {
+  findResponsibilityPointsByStudentId,
+  findResponsibilityPointsByStudentIdVariables,
+} from '../../schemaTypes'
+import { RESPONSIBILITY_POINTS_QUERY } from '../dashboard/teacher/student-information/general-student-information/ResponsibilityPointsDisplay'
 
 export type StudentGradeDisplayProps = { studentId: string }
 
@@ -22,12 +28,56 @@ export const StudentGradeDisplay = ({
     pollInterval: 5000,
     markingPeriod: currentMarkingPeriod.context.currentMarkingPeriod,
   })
-
+  const {
+    loading: responsibilityPointsLoading,
+    data: responsibilityPointsData,
+  } = useQuery<
+    findResponsibilityPointsByStudentId,
+    findResponsibilityPointsByStudentIdVariables
+  >(RESPONSIBILITY_POINTS_QUERY, {
+    variables: {
+      input: { studentId },
+    },
+    // onCompleted: (data) => console.log('polling'),
+    onError: (error) => console.error(error),
+  })
+  let responsibilityPoint: any = 0
+  if (!responsibilityPointsLoading && responsibilityPointsData) {
+    responsibilityPoint =
+      responsibilityPointsData.findResponsibilityPointsByStudentId.responsibilityPoints.find(
+        (rp) =>
+          rp.markingPeriod ===
+          currentMarkingPeriod.context.currentMarkingPeriod,
+      )
+  }
+  const responsibilityPoints = responsibilityPoint.responsibilityPoints
+  let extracredit =
+    responsibilityPoints >= 100 ? (responsibilityPoints - 100) / 10 : 0
+  console.log(extracredit)
   return (
     <>
-      <GradeDisplay>Current Grade</GradeDisplay>
-      <GradeDisplay>{!loading ? grade + '%' : 'Calculating'}</GradeDisplay>
-
+      <div
+        style={{
+          display: 'grid',
+          justifyItems: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <GradeDisplay>Current Grade</GradeDisplay>
+        <GradeDisplay>{!loading ? grade + '%' : 'Calculating'}</GradeDisplay>
+      </div>
+      <div
+        style={{
+          display: 'grid',
+          justifyItems: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <GradeDisplay>Extra Credit</GradeDisplay>
+        <GradeDisplay>
+          {!loading ? extracredit + 'Points' : 'Calculating'}
+        </GradeDisplay>
+      </div>
       {/* <div>
             This grade may not be what is in Genesis and will change constantly.
             If you have just completed an essay, your grade will change when
